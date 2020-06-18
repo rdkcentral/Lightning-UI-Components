efcb56767d88745d6b59f7415851c0005452d6d8
BJ Cantlupe <bjcantlupe@gmail.com>
2020-06-18 16:12:53 -0400

    docs: add theme and brand logo

---
+++ .storybook/theme.js
@@ -0,0 +1,12 @@
+import { create } from '@storybook/theming/create';
+import brand from './brand.png';
+
+export default create({
+  base: 'light',
+
+  appBg: 'rgb(33,	35,	42)',
+  textColor: 'white',
+  brandTitle: 'Lightning UI Components',
+  brandUrl: 'https://rdkcentral.github.io/Lightning/',
+  brandImage: brand,
+});
