23cbbf8ad934f22c210cc2ff4fb2a90208b36f4e
BJ Cantlupe <bjcantlupe@gmail.com>
2020-06-23 11:07:59 -0400

    docs: add initial Row story and README

---
+++ components/Row/Row.stories.js
@@ -0,0 +1,132 @@
+import lng from 'wpe-lightning';
+
+import Row from '.';
+
+export default {
+  title: 'Row',
+  component: Row
+};
+
+export const Basic = () =>
+  class Basic extends lng.Component {
+    static _template() {
+      return {
+        x: 20,
+        y: 20,
+        Row: {
+          type: Row,
+          items: [
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 }
+          ]
+        }
+      };
+    }
+
+    _getFocused() {
+      return this.tag('Row');
+    }
+  };
+
+export const Title = () =>
+  class Title extends lng.Component {
+    static _template() {
+      return {
+        x: 20,
+        y: 20,
+        Row: {
+          type: Row,
+          h: 40,
+          title: { fontSize: 20, text: 'Buncha Buttons' },
+          showTitle: true,
+          items: [
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 }
+          ]
+        }
+      };
+    }
+
+    _getFocused() {
+      return this.tag('Row');
+    }
+  };
+
+export const SideScrolling = () =>
+  class SideScrolling extends lng.Component {
+    static _template() {
+      return {
+        x: 20,
+        y: 20,
+        Row: {
+          type: Row,
+          w: 900,
+          alwaysScroll: true,
+          items: [
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 },
+            { type: Button, buttonText: 'Button', w: 150 }
+          ]
+        }
+      };
+    }
+
+    _getFocused() {
+      return this.tag('Row');
+    }
+  };
+
+export const VaryingItemWidth = () =>
+  class VaryingItemWidth extends lng.Component {
+    static _template() {
+      return {
+        x: 20,
+        y: 20,
+        Row: {
+          type: Row,
+          items: Array.apply(null, { length: 10 }).map((_, i) => ({
+            type: Button,
+            buttonText: 'Button',
+            w: 120 + Math.floor(Math.random() * 80)
+          }))
+        }
+      };
+    }
+
+    _getFocused() {
+      return this.tag('Row');
+    }
+  };
+
+class Button extends lng.Component {
+  static _template() {
+    return {
+      color: 0xff1f1f1f,
+      texture: lng.Tools.getRoundRect(150, 40, 4),
+      Label: {
+        x: 75,
+        y: 22,
+        mount: 0.5,
+        color: 0xffffffff,
+        text: { fontSize: 20 }
+      }
+    };
+  }
+  _init() {
+    this.tag('Label').text = this.buttonText;
+  }
+  _focus() {
+    this.color = 0xffffffff;
+    this.tag('Label').color = 0xff1f1f1f;
+  }
+  _unfocus() {
+    this.color = 0xff1f1f1f;
+    this.tag('Label').color = 0xffffffff;
+  }
+}
