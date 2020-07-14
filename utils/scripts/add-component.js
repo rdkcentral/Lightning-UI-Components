946a72fae253e0b26ddaba9587e3fcde10d53189
silava673 <Sukanya_Ilavarasi@comcast.com>
2020-07-14 23:36:35 +0530

    BLDKCMF-17 Lightning-UI-Components

--- utils/scripts/add-component.js
+++ utils/scripts/add-component.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /*
  * If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
@@ -16,8 +18,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#!/usr/bin/env node
 
 /* eslint-disable no-console */
 
