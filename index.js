eec602e409ab10fa1b5b83e1d8eb7cbaf7d55918
BJ Cantlupe <bjcantlupe@gmail.com>
2020-07-16 16:13:10 -0400

    feat: export top-level components

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 export { default as ActionButton } from './components/ActionButton/index.js';
+export { default as Column } from './components/Column/index.js';
+export { default as FocusManager } from './components/FocusManager/index.js';
+export { default as Pivot } from './components/Pivot/index.js';
+export { default as Row } from './components/Row/index.js';
+export { default as Tile } from './components/Tile/index.js';
