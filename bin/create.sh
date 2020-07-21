2ac323b699be96283b755336313e48aae37fd1ab
BJ Cantlupe <bjcantlupe@gmail.com>
2020-07-21 11:28:39 -0400

    feat(ListItemBase): add ListItemBase component (wip)

--- bin/create.sh
+++ bin/create.sh
@@ -1,3 +1,12 @@
+if [ -z "$1" ]
+then
+  echo "No component name found, try again with something like:"
+  echo ""
+  echo "npm run create MyComponent"
+  echo ""
+  exit 0
+fi
+
 mkdir -p ./components/$1
 touch ./components/$1/index.js ./components/$1/$1.test.js
 touch "./components/$1/$1.stories.js" || $(echo "Create a ./components/$1 folder then re-run this command");
