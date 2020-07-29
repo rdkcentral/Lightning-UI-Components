1060e20d867b1a52a3c58c957dc2b160a9a69136
Cantlupe, Benjamin (Contractor) <Benjamin_Cantlupe@comcast.com>
2020-07-29 09:56:55 -0400

    feat: expose ListItem and ListItemToggle in index (#59)
    * feat: expose ListItem and ListItemToggle in index
    
    * refactor: inline ListItem exports
    
    Co-authored-by: BJ Cantlupe <bjcantlupe@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ export { default as Announcer } from './components/Announcer/index.js';
 export { default as ActionButton } from './components/ActionButton/index.js';
 export { default as Column } from './components/Column/index.js';
 export { default as FocusManager } from './components/FocusManager/index.js';
+export { default as ListItem, ListItemToggle } from './components/ListItem/index.js';
 export { default as Pivot } from './components/Pivot/index.js';
 export { default as Row } from './components/Row/index.js';
 export { default as Tile } from './components/Tile/index.js';
