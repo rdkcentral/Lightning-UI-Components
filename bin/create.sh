394019263ac05dc39b2cc02bf2cb39a93bf16b31
Cantlupe, Benjamin (Contractor) <Benjamin_Cantlupe@comcast.com>
2020-07-30 14:59:54 -0400

    chore: add test and index templates to create script (#65)
    Co-authored-by: BJ Cantlupe <bjcantlupe@gmail.com>

--- bin/create.sh
+++ bin/create.sh
@@ -8,11 +8,14 @@ then
 fi
 
 mkdir -p ./components/$1
-touch ./components/$1/index.js ./components/$1/$1.test.js
-touch "./components/$1/$1.stories.js" || $(echo "Create a ./components/$1 folder then re-run this command");
+touch ./components/$1/index.js ./components/$1/$1.test.js ./components/$1/$1.stories.js
 
 STORY_TEMPLATE=$(cat "bin/templates/story.template")
 MDX_TEMPLATE=$(cat "bin/templates/readme.template")
+TEST_TEMPLATE=$(cat "bin/templates/test.template")
+INDEX_TEMPLATE=$(cat "bin/templates/index.template")
 
 echo "${STORY_TEMPLATE//MyComponent/$1}" > ./components/$1/$1.stories.js
 echo "${MDX_TEMPLATE//MyComponent/$1}" > ./components/$1/$1.mdx
+echo "${TEST_TEMPLATE//MyComponent/$1}" > ./components/$1/$1.test.js
+echo "${INDEX_TEMPLATE//MyComponent/$1}" > ./components/$1/index.js
