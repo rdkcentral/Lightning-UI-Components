81a5925c991bd0fdbd5fb10d509e5886a3cd2cc3
BJ Cantlupe <bjcantlupe@gmail.com>
2020-08-11 11:40:55 -0400

    feat: expose readied components (#77)
    Co-authored-by: BJ Cantlupe <bjcantlupe@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-export { default as Announcer } from './components/Announcer/index.js';
 export { default as ActionButton } from './components/ActionButton/index.js';
+export { default as Announcer } from './components/Announcer/index.js';
 export { default as Column } from './components/Column/index.js';
 export { default as FocusManager } from './components/FocusManager/index.js';
-export { default as ListItem, ListItemToggle } from './components/ListItem/index.js';
+export { default as ListItem, ListItemImage, ListItemToggle } from './components/ListItem/index.js';
 export { default as Pivot } from './components/Pivot/index.js';
 export { default as Row } from './components/Row/index.js';
+export { default as Slider } from './components/Slider/index.js';
 export { default as Tile } from './components/Tile/index.js';
+export { default as Toggle } from './components/Toggle/index.js';
