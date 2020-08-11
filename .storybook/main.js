c152ceee9ec0ccb19f123efa06a83af20a863649
BJ Cantlupe <bjcantlupe@gmail.com>
2020-08-11 16:17:45 -0400

    withTransitions and createTransitions (#79)
    * feat: create story to sandbox transitions
    
    * feat: add withTransitions mixin
    
    * feat: prove transitions can be applied on child classes
    
    * feat(withTransitions): swap options, add scale property
    
    * feat(withTransition): break out functionality to allow customization
    
    * fix: update transition patching, start docs for withTransition
    
    * docs(withTransition): first full draft
    
    * test(withTransitions): 100% coverage
    
    Co-authored-by: BJ Cantlupe <bjcantlupe@gmail.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,8 +8,14 @@ const lightningInspector = path.resolve(
 // Add file names to the 2nd glob to skip them in production
 const stories =
   process.env.NODE_ENV === 'development'
-    ? ['../components/**/*.stories.{js,mdx}']
-    : ['../components/**/!(ListItemBase).stories.{js,mdx}'];
+    ? [
+        '../components/**/*.stories.{js,mdx}',
+        '../mixins/*.stories.{js,mdx}'
+      ]
+    : [
+        '../components/**/!(ListItemBase).stories.{js,mdx}',
+        '../mixins/*.stories.{js,mdx}'
+      ];
 
 module.exports = {
   addons: [
---
+++ mixins/index.js
@@ -0,0 +1,41 @@
+import { createTransitions } from '../components/Styles/Transitions';
+
+const patchTransitions = (element, transitions) => {
+  if (!element._transitions) {
+    element.transitions = transitions;
+    return;
+  };
+
+  for (let key in transitions) {
+    if (!element._transitions[key]) {
+      element.transition(key, transitions[key]);
+    }
+  }
+};
+
+const collectChildren = (children, result=[]) => {
+  if (children && children.length) {
+    result.push(...children);
+    return collectChildren(children.flatMap(child => child.children), result);
+  }
+  return result
+}
+
+export const withTransitions = (Base, options = {}) => {
+  const transitions = createTransitions(options);
+
+  return class extends Base {
+    static _template() {
+      return {
+        transitions,
+        ...super._template()
+      };
+    }
+
+    _init() {
+      const flatTree = collectChildren(this.children);
+      flatTree.forEach(element => patchTransitions(element, transitions));
+      super._init();
+    }
+  };
+};
