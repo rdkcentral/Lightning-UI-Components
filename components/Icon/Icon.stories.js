5b4130fc05039e0005a101b7053de0812864630c
BJ Cantlupe <bjcantlupe@gmail.com>
2020-08-20 11:26:09 -0400

    feat: add Icon component (#88)
    * feat: add Icon component
    
    * test(Icon): add initial tests
    
    * docs(Icon): add initial docs
    
    Co-authored-by: BJ Cantlupe <bjcantlupe@gmail.com>

---
+++ components/Icon/Icon.stories.js
@@ -0,0 +1,71 @@
+import lng from 'wpe-lightning';
+
+import Icon from '.';
+import circle from '../../assets/images/circle.svg';
+import lightning from '../../assets/images/ic_lightning_white_32.png';
+import mdx from './Icon.mdx';
+
+export default {
+  title: 'Icon',
+  component: Icon,
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  }
+};
+
+export const Basic = () =>
+  class Basic extends lng.Component {
+    static _template() {
+      return {
+        Icon: {
+          type: Icon,
+          w: 50,
+          h: 50,
+          icon: circle
+        }
+      };
+    }
+
+    _getFocused() {
+      return this.tag('Icon');
+    }
+  };
+
+export const InlineSvg = () =>
+  class InlineSvg extends lng.Component {
+    static _template() {
+      return {
+        Icon: {
+          type: Icon,
+          w: 50,
+          h: 50,
+          icon:
+            '<svg xmlns="http://www.w3.org/2000/svg" height="100" width="100"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" /></svg>'
+        }
+      };
+    }
+
+    _getFocused() {
+      return this.tag('Icon');
+    }
+  };
+
+export const PNG = () =>
+  class PNG extends lng.Component {
+    static _template() {
+      return {
+        Icon: {
+          type: Icon,
+          w: 32,
+          h: 32,
+          icon: lightning
+        }
+      };
+    }
+
+    _getFocused() {
+      return this.tag('Icon');
+    }
+  };
