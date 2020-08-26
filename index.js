64d1e5c9b47c0fb0600eaf57548de74db2404bfc
Cantlupe, Benjamin (Contractor) <Benjamin_Cantlupe@comcast.com>
2020-08-26 11:28:12 -0400

    fix: try a different way of exporting styles (#105)
    Co-authored-by: BJ Cantlupe <bjcantlupe@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import * as Styles from './components/Styles/index.js';
+export { Styles };
+
 export { default as ActionButton } from './components/ActionButton/index.js';
 export { default as Announcer } from './components/Announcer/index.js';
 export { default as Column } from './components/Column/index.js';
@@ -7,7 +10,6 @@ export { default as Pivot } from './components/Pivot/index.js';
 export { default as ProgressBar } from './components/ProgressBar/index.js';
 export { default as Row } from './components/Row/index.js';
 export { default as Slider } from './components/Slider/index.js';
-export * as Styles from './components/Styles/index.js';
 export { default as Tile } from './components/Tile/index.js';
 export { default as Toggle } from './components/Toggle/index.js';
 export { default as Tooltip } from './components/Tooltip/index.js';
