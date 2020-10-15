7c85d34f55d93aab5fd5e2fee54ba02c4b1a4dd6
Simek, Chelsea <Chelsea_Simek@comcast.com>
2020-10-15 16:58:35 -0400

    feat/radio (#186)
    * feat(radio): radio component with checked functionality
    
    * test(radio): radio tests
    
    * fix(radio): conflicts
    
    * docs(radio): update docs

---
+++ components/Radio/Radio.test.js
@@ -0,0 +1,30 @@
+import TestUtils from '../lightning-test-utils';
+import Radio from '.';
+
+const createRadio = TestUtils.makeCreateComponent(Radio);
+
+describe('Radio', () => {
+  let radio, testRenderer;
+
+  beforeEach(() => {
+    [radio, testRenderer] = createRadio();
+  });
+  afterEach(() => {
+    radio = null;
+    testRenderer = null;
+  });
+
+  it('renders', () => {
+    const tree = testRenderer.toJSON(2);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('toggles checked state on enter', () => {
+    [radio, testRenderer] = createRadio({ checked: false });
+    expect(radio.checked).toEqual(false);
+    testRenderer.keyPress('Enter');
+    expect(radio.checked).toEqual(true);
+    testRenderer.keyPress('Enter');
+    expect(radio.checked).toEqual(false);
+  });
+});
