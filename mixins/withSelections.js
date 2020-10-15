4c20ad0edc4dbf58f43eda6944873d15e497ede0
BJ Cantlupe <bjcantlupe@gmail.com>
2020-10-15 15:36:59 -0400

    feat: add selection manager (#180)
    * feat: add selection manager
    
    * feat: add selection manager
    
    * remove _withSelections
    
    * cleanup _captureKey
    
    * re-organize options
    
    Co-authored-by: BJ Cantlupe <bjcantlupe@gmail.com>
    Co-authored-by: Simek, Chelsea <Chelsea_Simek@comcast.com>

---
+++ mixins/withSelections.js
@@ -0,0 +1,77 @@
+export default function withSelections(Base, options = {}) {
+  const defaultOpts = {
+    captureKey: 'Enter',
+    defaultIndex: 0,
+    eventName: 'onSelect',
+    keys: {
+      currentSelected: 'currentSelected',
+      currentSelectedIndex: 'currentSelectedIndex',
+      isSelected: 'isSelected',
+      items: 'items',
+      setSelected: 'setSelected',
+    },
+  };
+  const {
+    captureKey,
+    defaultIndex,
+    eventName,
+    keys: {
+      currentSelected,
+      currentSelectedIndex,
+      isSelected,
+      items,
+      setSelected
+    }
+  } = {
+    ...defaultOpts,
+    ...options,
+    keys: {
+      ...defaultOpts.keys,
+      ...options.keys
+    }
+  };
+
+  return class extends Base {
+    static get name() { return Base.name }
+
+    _construct() {
+      super._construct();
+      this[currentSelectedIndex] = defaultIndex
+    }
+
+    _init() {
+      super._init();
+      this[currentSelected][isSelected] = true;
+    }
+
+    [setSelected](selected, val) {
+      const prevSelected = (this[items] || [])[this[currentSelectedIndex]];
+
+      if (selected && (selected !== prevSelected || selected[isSelected] !== val)) {
+        prevSelected && (prevSelected[isSelected] = false);
+        selected[isSelected] = val;
+        this[currentSelectedIndex] = Array.from(this[items] || []).indexOf(selected);
+        this.fireAncestors('$' + eventName, selected, val);
+      }
+    }
+
+    _captureKey(event) {
+      if (
+        (Array.isArray(captureKey) && captureKey.indexOf(event.key) > -1)
+        || event.key === captureKey
+      ) {
+        const [selected] = this.stage.application.focusPath.slice(-1);
+        this[setSelected](selected, true);
+      }
+      return false
+    }
+
+    ['$' + setSelected](selected, val) {
+      this[setSelected](selected, val);
+    }
+
+    get [currentSelected]() {
+      return this[items][this[currentSelectedIndex]];
+    }
+  }
+}
---
+++ mixins/withSelections.test.js
@@ -0,0 +1,156 @@
+import lng from 'wpe-lightning';
+import TestUtils from '../components/lightning-test-utils';
+import withSelections from './withSelections';
+
+const duplicate = (object, quantity) => Array.apply(null, { length: quantity }).map(() => Object.assign({}, object));
+
+describe('selectionManager', () => {
+  let WithSelections, testRenderer;
+  class Example extends lng.Component {
+    static _template() {
+      return {
+        items: duplicate({ type: lng.Component }, 3),
+      }
+    }
+
+    _construct() {
+      this._currentFocusedIndex = 0;
+    }
+
+    _getFocused() {
+      return this.currentFocused || this;
+    }
+
+    _handleLeft() {
+      if (this.currentFocusedIndex > 0) {
+        this.currentFocusedIndex -= 1;
+        this._refocus();
+      }
+    }
+
+    _handleRight() {
+      if (this.currentFocusedIndex < this.items.length - 1) {
+        this.currentFocusedIndex += 1;
+        this._refocus();
+      }
+    }
+
+    get currentFocused() {
+      return this.items[this.currentFocusedIndex]
+    }
+
+    get currentFocusedIndex() {
+      return this._currentFocusedIndex;
+    }
+
+    set currentFocusedIndex(v) {
+      this._currentFocusedIndex = v
+    }
+
+    get items() {
+      return this.children;
+    }
+
+    set items(v) {
+      this.children = v;
+    }
+  }
+
+  beforeEach(() => {
+    [WithSelections, testRenderer] = TestUtils.makeCreateComponent(withSelections(Example))();
+  });
+
+  afterEach(() => {
+    WithSelections = null;
+  });
+
+  it('extends the base class', () => {
+    expect(WithSelections.constructor.name).toBe('Example');
+  });
+
+  it('manages selected state for components', () => {
+    const [c1, c2, c3] = WithSelections.items;
+    expect(c1.isSelected).toBe(true);
+    testRenderer.keyPress('Right');
+    expect(WithSelections._getFocused()).toBe(c2);
+    expect(WithSelections.currentSelected).toBe(c1);
+    testRenderer.keyPress('Enter');
+    expect(WithSelections.currentFocused).toBe(c2);
+    expect(c1.isSelected).toBe(false);
+    expect(c2.isSelected).toBe(true);
+
+    c3.fireAncestors('$setSelected', c3, true);
+    expect(c3.isSelected).toBe(true);
+  });
+
+  it('fires an event when a selected value changes', () => {
+    jest.spyOn(WithSelections, 'fireAncestors');
+    WithSelections.setSelected(WithSelections.items[0], true);
+    expect(WithSelections.fireAncestors).not.toBeCalled();
+
+    WithSelections.setSelected(WithSelections.items[1], true);
+    expect(WithSelections.fireAncestors).toBeCalledWith('$onSelect', WithSelections.items[1], true);
+  });
+
+  it('supports custom properties', () => {
+    class Custom extends lng.Component {
+      static _template() {
+        return {
+          childs: duplicate({}, 3)
+        }
+      }
+    }
+    [WithSelections, testRenderer] = TestUtils.makeCreateComponent(withSelections(Custom, {
+      keys: {
+        currentSelected: 'current',
+        currentSelectedIndex: 'currentIndex',
+        items: 'childs',
+        isSelected: 'selected',
+        setSelected: 'toggleSelected',
+      },
+      captureKey: ['Enter', '1'],
+      eventName: 'select'
+    }))();
+    expect(WithSelections.currentIndex).toBe(0);
+    expect(WithSelections.current).toBe(WithSelections.childs[0]);
+    expect(WithSelections.current.selected).toBe(true);
+    expect(typeof WithSelections.toggleSelected).toBe('function');
+    jest.spyOn(WithSelections, 'fireAncestors');
+    WithSelections.toggleSelected(WithSelections.current, false);
+    expect(WithSelections.fireAncestors).toBeCalledWith('$select', WithSelections.current, false);
+    testRenderer.keyPress('1');
+    expect(WithSelections.fireAncestors).toBeCalledTimes(2)
+  });
+
+  it('preserves callbacks', () => {
+    const addSelected = jest.fn();
+    const removeSelected = jest.fn();
+
+    class Callbacks extends lng.Component {
+      static _template() {
+        return {
+          items: [{ addSelected, removeSelected }]
+        }
+      }
+    }
+    [WithSelections, testRenderer] = TestUtils.makeCreateComponent(withSelections(Callbacks))();
+
+    WithSelections.items[0].addSelected();
+    expect(addSelected).toBeCalled();
+
+    WithSelections.items[0].removeSelected();
+    expect(addSelected).toBeCalled();
+  });
+
+  it('preserves initial selected state', () => {
+    class InitialValue extends lng.Component {
+      static _template() {
+        return {
+          items: [{ selected: true }]
+        }
+      }
+    }
+    [WithSelections, testRenderer] = TestUtils.makeCreateComponent(withSelections(InitialValue))();
+    expect(WithSelections.items[0].selected).toBe(true);
+  });
+});
