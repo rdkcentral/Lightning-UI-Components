3e71413f64082c4052410c44f1ec17ce3a9915a6
BJ Cantlupe <bjcantlupe@gmail.com>
2020-10-21 11:43:36 -0400

    fix(Button): add to main index, clarify button docs (#200)
    Co-authored-by: BJ Cantlupe <bjcantlupe@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export * from './components/Styles/index.js';
 export { default as ActionButton } from './components/ActionButton/index.js';
 export { default as Announcer } from './components/Announcer/index.js';
+export { default as Button } from './components/Button/index.js';
 export { default as Column } from './components/Column/index.js';
 export { default as FocusManager } from './components/FocusManager/index.js';
 export { default as FocusRing } from './components/FocusRing/index.js';
