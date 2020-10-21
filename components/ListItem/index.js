8ecd3f7a39f3a841dea15b582d5cc564352cc4aa
Simek, Chelsea <Chelsea_Simek@comcast.com>
2020-10-21 16:25:05 -0400

    feat/List Item Radio (#196)
    * feat(radio): radio component with checked functionality
    
    * test(radio): radio tests
    
    * feat(listitemradio): composed component for list item with radio
    
    * feat(listitemradio): update export

--- components/ListItem/index.js
+++ components/ListItem/index.js
@@ -10,6 +10,7 @@ import Icon from '../Icon';
 import Slider from '../Slider';
 import Tile from '../Tile';
 import Toggle from '../Toggle';
+import Radio from '../Radio';
 import { RoundRect, getFirstNumber } from '../../utils';
 import withStyles from '../../mixins/withStyles';
 
@@ -282,6 +283,41 @@ export class ListItemToggle extends ListItem {
 
   get _Toggle() {
     return this.tag('Container').tag('Toggle');
+  }
+}
+
+export class ListItemRadio extends ListItem {
+  _init() {
+    super._init();
+    this.icon = undefined;
+    this._Right.patch({
+      w: 32,
+      h: 32,
+      Radio: {
+        type: Radio,
+        checked: this.checked
+      }
+    });
+  }
+
+  isChecked() {
+    return Boolean(this._Radio.checked);
+  }
+
+  toggle() {
+    return this._Radio.toggle();
+  }
+
+  _getFocused() {
+    return this._Radio;
+  }
+
+  _handleEnter() {
+    this._Radio._handleEnter();
+  }
+
+  get _Radio() {
+    return this.tag('Container').tag('Radio');
   }
 }
 
