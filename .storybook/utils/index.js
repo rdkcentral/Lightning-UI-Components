6760132e90a65d80b8210dedddf8a95f7710faf4
BJ Cantlupe <bjcantlupe@gmail.com>
2020-12-09 14:50:09 -0500

    docs: add API docs for components using withStyles

--- .storybook/utils/index.js
+++ .storybook/utils/index.js
@@ -46,3 +46,67 @@ export const ObjectFormat = ({ object }) => {
 export const GenericType = ({ children, type }) => {
   return [type, '<', children, '>'].map((x, i) => <Item key={i}>{x}</Item>);
 };
+
+export const Theme = ({ theme={}, isColor=false }) => {
+  /** Naively parses a theme object to generate value documentation */
+  const getValue = ([key, value], isColor=false) => {
+    console.log('key', key, 'value', value, 'isColor', isColor)
+    if (key === '__isColor') return false;
+    switch (typeof value) {
+      case 'function':
+        return <li key={key}>{`${key}: function()`}</li>;
+      case 'string':
+        return <li key={key}>{key}: "{value}"</li>;
+      case 'object':
+        return (
+          <li key={key}>
+            <details>
+              <summary>{key}: Object</summary>
+              <ul style={{ listStyleType: 'none' }}>
+              {Object.entries(value).map(entries => getValue(entries, '__isColor' in value))}
+              </ul>
+            </details>
+          </li>
+        );
+      default:
+        // converts value to color string if it is a sibling of `__isColor: true` or
+        // if "color" is in the key name
+        return (
+          <li key={key}>
+            {key}: {isColor || key.match(/color/i) ? (
+              <>
+              <span>
+                0x{value.toString(16).toUpperCase()}
+              </span>
+              <div style={{
+                display: 'inline-block',
+                width: '20px',
+                height: '20px',
+                margin: '-4px 4px',
+                backgroundColor: `#${value.toString(16).slice(2)}`,
+                opacity: parseInt('0x' + value.toString(16).slice(0,2)) / 255
+              }}></div>
+              </>
+            ) : value}
+          </li>
+        );
+    }
+  };
+  const style = {
+    fontFamily: `"Nunito Sans",-apple-system,".SFNSText-Regular","San Francisco",BlinkMacSystemFont,"Segoe UI","Helvetica Neue",Helvetica,Arial,sans-serif`,
+    margin:0,
+    fontSize: '14px',
+    color: 'white',
+    lineHeight: '24px',
+    listStyleType: 'none'
+  }
+
+  const liStyle = {
+    marginTop: '.25em'
+  }
+  return (
+    <ul style={style}>
+    {Object.entries(theme).map(entries => getValue(entries, isColor))}
+    </ul>
+  )
+}
