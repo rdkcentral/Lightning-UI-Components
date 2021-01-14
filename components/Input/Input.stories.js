92a0be46027dad032a1b4edc95058f0f484ffa39
Tinsley, Ollie (Contractor) <Ollie_Tinsley@comcast.com>
2021-01-14 16:38:10 -0500

    docs(input): update input stories/documentation (#323)
    * docs(input): update input stories/documentation
    
    * docs(input): update caption icon size and snapshot
    
    * docs(input): remove caption from joint story
    
    * docs(input): add icon h/w to withStyles
    
    * docs(input): update docs
    
    * docs(input): run lint on code merged from main

--- components/Input/Input.stories.js
+++ components/Input/Input.stories.js
@@ -22,11 +22,117 @@ export const Basic = args =>
         Input: {
           w: 500,
           type: Input,
+          placeholder: args.placeholder
+        }
+      };
+    }
+
+    _handleKey(event) {
+      const { key } = event;
+      if (key.length === 1) {
+        this.tag('Input').insert(key);
+      }
+      if (key === 'Backspace') {
+        this.tag('Input').backspace();
+      }
+    }
+
+    _getFocused() {
+      if (args.focused) {
+        return this.tag('Input');
+      }
+    }
+  };
+
+Basic.args = {
+  placeholder: 'Type something'
+};
+Basic.argTypes = {
+  focused: { control: 'boolean' },
+  listening: { control: 'boolean' }
+};
+Basic.parameters = {
+  argActions: {
+    focused: (isFocused, component) => {
+      component._getFocused = isFocused
+        ? () => component.tag('Input')
+        : () => {};
+      component._refocus();
+    },
+    listening: (isListening, component) => {
+      component.tag('Input').listening = isListening;
+    }
+  }
+};
+
+export const Caption = args =>
+  class Caption extends lng.Component {
+    static _template() {
+      return {
+        Input: {
+          w: 500,
+          type: Input,
           placeholder: args.placeholder,
-          password: args.password,
-          mask: args.mask,
-          icon: args.icon,
-          caption: args.caption,
+          caption: [
+            'This is a caption using an array',
+            {
+              icon:
+                'http://myriad.merlin.comcast.com/select/logo?entityId=8527084350383982239&width=32&height=&ratio=1x1&trim=false',
+              title: 'Rotten Tomatoes rating'
+            },
+            'with an image'
+          ]
+        }
+      };
+    }
+
+    _handleKey(event) {
+      const { key } = event;
+      if (key.length === 1) {
+        this.tag('Input').insert(key);
+      }
+      if (key === 'Backspace') {
+        this.tag('Input').backspace();
+      }
+    }
+
+    _getFocused() {
+      if (args.focused) {
+        return this.tag('Input');
+      }
+    }
+  };
+
+Caption.args = {
+  placeholder: 'Type something'
+};
+Caption.argTypes = {
+  focused: { control: 'boolean' },
+  listening: { control: 'boolean' }
+};
+Caption.parameters = {
+  argActions: {
+    focused: (isFocused, component) => {
+      component._getFocused = isFocused
+        ? () => component.tag('Input')
+        : () => {};
+      component._refocus();
+    },
+    listening: (isListening, component) => {
+      component.tag('Input').listening = isListening;
+    }
+  }
+};
+
+export const Icon = args =>
+  class Basic extends lng.Component {
+    static _template() {
+      return {
+        Input: {
+          w: 500,
+          type: Input,
+          placeholder: args.placeholder,
+          icon: eye,
           iconColor: rgba2argb(args.iconColor),
           iconFocusColor: rgba2argb(args.iconFocusColor)
         }
@@ -50,25 +156,146 @@ export const Basic = args =>
     }
   };
 
-Basic.args = {
+Icon.args = {
+  placeholder: 'Type something',
+  iconColor: 'rgba(98,98,98,1)',
+  iconFocusColor: 'rgba(13,13,15,1)'
+};
+Icon.argTypes = {
+  focused: { control: 'boolean' },
+  listening: { control: 'boolean' },
+  iconColor: { control: 'color' },
+  iconFocusColor: { control: 'color' }
+};
+Icon.parameters = {
+  argActions: {
+    focused: (isFocused, component) => {
+      component._getFocused = isFocused
+        ? () => component.tag('Input')
+        : () => {};
+      component._refocus();
+    },
+    listening: (isListening, component) => {
+      component.tag('Input').listening = isListening;
+    },
+    iconColor: (iconColor, component) => {
+      component.tag('Input').iconColor = rgba2argb(iconColor);
+    },
+    iconFocusColor: (iconFocusColor, component) => {
+      component.tag('Input').iconFocusColor = rgba2argb(iconFocusColor);
+    }
+  }
+};
+
+export const Password = args =>
+  class Password extends lng.Component {
+    static _template() {
+      return {
+        Input: {
+          w: 500,
+          type: Input,
+          placeholder: args.placeholder,
+          password: args.password,
+          mask: args.mask
+        }
+      };
+    }
+
+    _handleKey(event) {
+      const { key } = event;
+      if (key.length === 1) {
+        this.tag('Input').insert(key);
+      }
+      if (key === 'Backspace') {
+        this.tag('Input').backspace();
+      }
+    }
+
+    _getFocused() {
+      if (args.focused) {
+        return this.tag('Input');
+      }
+    }
+  };
+
+Password.args = {
+  placeholder: 'Type something',
+  password: false,
+  mask: '•'
+};
+Password.argTypes = {
+  focused: { control: 'boolean' },
+  listening: { control: 'boolean' },
+  password: { control: 'boolean' },
+  mask: { control: 'text' }
+};
+Password.parameters = {
+  argActions: {
+    focused: (isFocused, component) => {
+      component._getFocused = isFocused
+        ? () => component.tag('Input')
+        : () => {};
+      component._refocus();
+    },
+    listening: (isListening, component) => {
+      component.tag('Input').listening = isListening;
+    },
+    password: (isPassword, component) => {
+      component.tag('Input').password = isPassword;
+    }
+  }
+};
+
+export const PasswordWithIcon = args =>
+  class PasswordWithIcon extends lng.Component {
+    static _template() {
+      return {
+        Input: {
+          w: 500,
+          type: Input,
+          placeholder: args.placeholder,
+          password: args.password,
+          mask: args.mask,
+          icon: eye,
+          iconColor: rgba2argb(args.iconColor),
+          iconFocusColor: rgba2argb(args.iconFocusColor)
+        }
+      };
+    }
+
+    _handleKey(event) {
+      const { key } = event;
+      if (key.length === 1) {
+        this.tag('Input').insert(key);
+      }
+      if (key === 'Backspace') {
+        this.tag('Input').backspace();
+      }
+    }
+
+    _getFocused() {
+      if (args.focused) {
+        return this.tag('Input');
+      }
+    }
+  };
+
+PasswordWithIcon.args = {
   placeholder: 'Type something',
   password: false,
   mask: '•',
   iconColor: 'rgba(98,98,98,1)',
-  iconFocusColor: 'rgba(13,13,15,1)',
-  caption: 'This is a caption',
-  icon: eye
+  iconFocusColor: 'rgba(13,13,15,1)'
 };
-Basic.argTypes = {
+PasswordWithIcon.argTypes = {
   focused: { control: 'boolean' },
   listening: { control: 'boolean' },
   password: { control: 'boolean' },
   mask: { control: 'text' },
-  hideIcon: { control: 'boolean' },
   iconColor: { control: 'color' },
   iconFocusColor: { control: 'color' }
 };
-Basic.parameters = {
+PasswordWithIcon.parameters = {
   argActions: {
     focused: (isFocused, component) => {
       component._getFocused = isFocused
@@ -82,10 +309,6 @@ Basic.parameters = {
     password: (isPassword, component) => {
       component.tag('Input').password = isPassword;
       component.tag('Input').icon = isPassword ? eyeHide : eye;
-    },
-    hideIcon: (isHide, component) => {
-      const icon = component.tag('Input').password ? eyeHide : eye;
-      component.tag('Input').icon = isHide ? null : icon;
     },
     iconColor: (iconColor, component) => {
       component.tag('Input').iconColor = rgba2argb(iconColor);
