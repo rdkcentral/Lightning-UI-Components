518ffcd141ca7a3b0e69a0a099cab19e93d00125
Lorenzo, Christopher <Christopher_Lorenzo@Comcast.com>
2021-01-26 12:03:03 -0500

    feat: support using global theme object (#338)

--- mixins/withStyles.js
+++ mixins/withStyles.js
@@ -1,9 +1,12 @@
+import lng from 'wpe-lightning';
 import createStyles from '../components/Styles/createStyles';
 import { clone } from '../utils';
 import { getXfinityTheme } from '../components/Styles/createTheme';
 
+lng.uiTheme = lng.uiTheme || getXfinityTheme();
+
 export default function withStyles(Base, styles, theme) {
-  const _theme = theme || Base.theme || getXfinityTheme();
+  const _theme = theme || Base.theme || lng.uiTheme;
   const _styles = Base.styles ? clone(Base.styles, createStyles(styles, _theme)) : createStyles(styles, _theme);
 
   return class extends Base {
