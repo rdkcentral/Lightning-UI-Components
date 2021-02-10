dbfdf725c717ee8851af485aa37dda3855435066
Ollie Tinsley <ollie.tinsley@gmail.com>
2021-02-10 14:35:44 -0500

    feat(focusRing): change focus ring style to match prototype (#363)
    * feat(focusRing): change focus ring style to match prototype
    
    * feat(focusRing): change spacing in stories
    
    * feat(focusRing): fix typo
    
    * feat(focusRing): pr changes
    
    * feat(focusRing): use whenEnabled before animation start
    
    * feat(focusRing): cleanup
    
    * feat(focusRing): pr changes
    
    * feat(focusRing): pr changes
    
    * feat(focusRing): update snapshot
    
    * feat(focusRing): save point switch to focus manager
    
    * feat(focusRing): save point switch to focus manager
    
    * feat(focusRing): code cleanup
    
    * feat(focusRing): code cleanup pr changes
    
    * feat(focusRing): remove console log
    
    * feat(focusRing): add unit tests
    
    * feat(focusRing): remove console log
    
    * feat(focusRing): update DataItem and Tile
    
    * feat(focusRing): pr changes
    
    * feat(focusRing): add radius from theme
    
    * feat(focusRing): remove unused import
    
    * feat(focusRing): add animate to stories and docs
    
    * feat(focusRing): start animation as default in story
    
    * feat(focusRing): change variable names for clarity
    
    * feat(focusRing): update unit test
    
    * feat(focusRing): flip gradient
    
    Co-authored-by: Rautenberg, Emily <Emily_Rautenberg@comcast.com>

--- components/FocusRing/FocusRing.stories.js
+++ components/FocusRing/FocusRing.stories.js
@@ -8,7 +8,10 @@ import kabob from '../../assets/images/kabob_320x180.jpg';
 export default {
   args: {
     size: 16,
-    radius: 8
+    radius: 8,
+    color: 'rgba(236,236,242,.96)',
+    secondaryColor: 'rgba(255,255,255,.08)',
+    animate: true
   },
   argTypes: {
     size: {
@@ -52,11 +55,38 @@ export const Basic = args =>
           h: 180,
           radius: 8,
           size: 8,
-          color: getHexColor(COLORS_NEUTRAL.light2, 8)
+          color: args.color,
+          secondaryColor: args.secondaryColor
         }
       };
     }
+
+    _init() {
+      this._FocusRing.startAnimation();
+    }
+
+    get _FocusRing() {
+      return this.tag('FocusRing');
+    }
   };
+
+Basic.argTypes = {
+  color: { name: 'Color', control: { type: 'color' } },
+  secondaryColor: { name: 'Secondary Color', control: { type: 'color' } },
+  animate: { control: 'boolean' }
+};
+Basic.parameters = {
+  argActions: {
+    animate: (play, component) => {
+      if (play) {
+        component._FocusRing.startAnimation();
+      } else {
+        component._FocusRing.stopAnimation();
+      }
+      component._refocus();
+    }
+  }
+};
 
 export const WithImage = args =>
   class Basic extends lng.Component {
@@ -68,7 +98,8 @@ export const WithImage = args =>
           src: kabob,
           w: 320,
           h: 180,
-          zIndex: 2
+          zIndex: 2,
+          radius: 8
         },
         FocusRing: {
           type: FocusRing,
