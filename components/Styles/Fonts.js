fd87d0131951621b386b130873461acd26e636da
Chelsea Simek <chelsea.z.simek@gmail.com>
2021-03-01 14:19:35 -0500

    feat(tokens): rollback tokens (#396)
    * fix(tokens): make a dependency vs devDependency
    
    * feat(tokens): roll back pulling in tokens package

--- components/Styles/Fonts.js
+++ components/Styles/Fonts.js
@@ -3,7 +3,6 @@
  *
  * Contains global font style information to easily maintain consistency throughout components.
  */
-import tokens from 'xfinity-ds-tokens/dist/lightning-ui-tokens/lightning/lightning-ui';
 import { flatten } from '../../utils';
 
 export const XfinityFonts = {
@@ -70,11 +69,102 @@ export const createFonts = (...fonts) =>
  * Xfinity-branded typography definitions for Lightning
  * https://share.goabstract.com/06d0601d-b7d2-48f3-93a2-17cc78bedb5e?mode=build&sha=6d9d06ec3e924eb83ff133826956e1e9a4fc7e36
  */
-// eventually going to add this at the token level,
-// but there is still dicussion around where `vertialAlign`
-// middle should live
-let typeTokens = Object.assign(tokens.typography, {});
-Object.keys(typeTokens).forEach(key => {
-  typeTokens[key].verticalAlign = 'middle';
-});
-export const TYPOGRAPHY = typeTokens;
+
+export const TYPOGRAPHY = {
+  display1: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 56,
+    fontWeight: 700,
+    letterSpacing: -0.4,
+    lineHeight: 72,
+    verticalAlign: 'middle'
+  },
+  display2: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 48,
+    fontWeight: 700,
+    letterSpacing: -0.4,
+    lineHeight: 64,
+    verticalAlign: 'middle'
+  },
+  headline1: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 36,
+    fontWeight: 700,
+    letterSpacing: -0.2,
+    lineHeight: 48,
+    verticalAlign: 'middle'
+  },
+  headline2: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 32,
+    fontWeight: 700,
+    letterSpacing: -0.2,
+    lineHeight: 40,
+    verticalAlign: 'middle'
+  },
+  headline3: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 28,
+    fontWeight: 700,
+    letterSpacing: -0.2,
+    lineHeight: 36,
+    verticalAlign: 'middle'
+  },
+  body1: {
+    fontFace: 'XfinityStandardMedium',
+    fontSize: 28,
+    fontWeight: 500,
+    letterSpacing: 0,
+    lineHeight: 40,
+    verticalAlign: 'middle'
+  },
+  body2: {
+    fontFace: 'XfinityStandardMedium',
+    fontSize: 24,
+    fontWeight: 500,
+    letterSpacing: 0,
+    lineHeight: 32,
+    verticalAlign: 'middle'
+  },
+  body3: {
+    fontFace: 'XfinityStandardMedium',
+    fontSize: 24,
+    fontWeight: 500,
+    letterSpacing: 0,
+    lineHeight: 32,
+    verticalAlign: 'middle'
+  },
+  button1: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 28,
+    fontWeight: 700,
+    letterSpacing: -0.2,
+    lineHeight: 32,
+    verticalAlign: 'middle'
+  },
+  button2: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 24,
+    fontWeight: 700,
+    letterSpacing: -0.2,
+    lineHeight: 32,
+    verticalAlign: 'middle'
+  },
+  callout1: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 14,
+    fontWeight: 700,
+    letterSpacing: 2,
+    lineHeight: 32,
+    verticalAlign: 'middle'
+  },
+  caption: {
+    fontFace: 'XfinityBrownBold',
+    fontSize: 20,
+    fontWeight: 700,
+    letterSpacing: 0,
+    lineHeight: 24,
+    verticalAlign: 'middle'
+  }
+};
