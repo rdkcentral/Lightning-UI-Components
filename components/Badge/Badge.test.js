37b83b77fb52e092e460054f09cd229fa4f31ccc
Rautenberg, Emily <Emily_Rautenberg@comcast.com>
2021-03-12 16:57:41 -0500

    feat(Badge): add icon support (#412)
    * feat(Badge): add icon support to badge component
    
    * build: fresh npm install
    
    * build: update package lock

--- components/Badge/Badge.test.js
+++ components/Badge/Badge.test.js
@@ -6,6 +6,8 @@ const createBadge = TestUtils.makeCreateComponent(Badge);
 
 describe('Badge', () => {
   let badge, testRenderer;
+  const lightningPath = 'assets/images/ic_lightning_white_32.png';
+  const circlePath = 'assets/images/circle.svg';
 
   beforeEach(() => {
     [badge, testRenderer] = createBadge();
@@ -16,6 +18,23 @@ describe('Badge', () => {
   });
 
   it('renders', () => {
+    const tree = testRenderer.toJSON(2);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders icon and text side by side', () => {
+    const [_, testRenderer] = createBadge({
+      title: 'HD',
+      icon: lightningPath
+    });
+    const tree = testRenderer.toJSON(2);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders icon only', () => {
+    const [_, testRenderer] = createBadge({
+      icon: lightningPath
+    });
     const tree = testRenderer.toJSON(2);
     expect(tree).toMatchSnapshot();
   });
@@ -58,5 +77,46 @@ describe('Badge', () => {
     expect(badge.padding).toBe(8);
     badge.padding = 50;
     expect(badge.padding).toBe(50);
+  });
+
+  it('should update the icon', () => {
+    const icon = circlePath;
+    [badge, testRenderer] = createBadge({ icon });
+    expect(badge.icon).toBe(icon);
+
+    const icon2 = lightningPath;
+    badge.icon = icon2;
+    expect(badge.icon).toBe(icon2);
+  });
+
+  it('should render the text and icon', () => {
+    const title = 'HD';
+    const icon = circlePath;
+
+    [badge, testRenderer] = createBadge({ title, icon });
+
+    expect(badge._BadgeText.text.text).toBe(title);
+    expect(badge._Icon.icon).toBe(icon);
+  });
+
+  it('should position the text and icon based on the iconAlign property', () => {
+    const title = 'HD';
+    const icon = circlePath;
+
+    [badge, testRenderer] = createBadge({
+      title,
+      icon,
+      iconAlign: 'right',
+      iconWidth: 50,
+      iconHeight: 50
+    });
+
+    testRenderer.update();
+    badge._BadgeText.loadTexture();
+    badge._Icon.loadTexture();
+
+    expect(badge._Icon.x).toEqual(
+      badge._BadgeText.x + badge._BadgeText.renderWidth
+    );
   });
 });
