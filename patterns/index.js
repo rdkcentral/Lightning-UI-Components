38bab379e6a646e336f01892f3d25e0c5fd57c07
Ollie Tinsley <ollie.tinsley@gmail.com>
2021-04-12 14:27:02 -0400

    feat(SportsStats): add stats pattern (#444)
    * feat(Stats): add stats pattern
    
    * feat(Stats): add arrow color, update snapshot
    
    * feat(Stats): clean up announce and change single to use groups
    
    * feat(Stats): update docs and test
    
    * feat(Stats): add single as object, update docs and test
    
    * feat(Stats): change fractional to percentage
    
    * feat(Stats): change single teams to group with 1 entity
    
    * feat(Stats): change groups to items for consistency
    
    * feat(Stats): add support for multiple color arg types
    
    * feat(Stats): remove get theme use theme from withStyles
    
    * feat(Stats): update docs
    
    * feat(Stats): update docs and export name
    
    * feat(Stats): remove export from index exists in patterns
    
    * feat(Stats): update docs
    
    * feat(Stats): update docs, names, snapshots

--- patterns/index.js
+++ patterns/index.js
@@ -4,4 +4,5 @@ export { default as ListItem } from './ListItem';
 export { default as MetadataCard } from './MetadataCard';
 export { default as MetadataTile } from './MetadataTile';
 export { default as Notification } from './Notification';
+export { default as SportStats } from './SportsStats';
 export { default as withMetadata } from './withMetadata';
