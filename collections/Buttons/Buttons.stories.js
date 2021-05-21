ec9049361736ebcbac1544af75f483d3ccc77ca9
Howenstine, Joshua (Contractor) <Joshua_Howenstine@comcast.com>
2021-05-21 14:12:50 -0700

    fix(ButtonsCollection): Add descriptive Labels to each button (#488)
    * fix(ButtonsCollection): update labels. Add ActionButtonXsmall. Doc link
    
    * fix(ButtonsCollection): update button collection labels and organization
    
    Co-authored-by: Simek, Chelsea <Chelsea_Simek@comcast.com>

--- collections/Buttons/Buttons.stories.js
+++ collections/Buttons/Buttons.stories.js
@@ -1,20 +1,23 @@
 import lng from '@lightningjs/core';
 import mdx from './Buttons.mdx';
-import icApps from '../../assets/images/ic_apps_white_32.png';
-import icOverflowVertical from '../../assets/images/ic_overflow_vertical_white_32.png';
-import icPlay from '../../assets/images/ic_play_white_32.png';
+import withStyles from '../../mixins/withStyles';
 import Column from '../../layout/Column';
-import Row from '../../layout/Row';
-import CollectionGroup from '../CollectionGroup';
 import {
   ActionButtonLarge,
   ActionButtonMedium,
   ActionButtonSmall,
   ActionButtonIconMedium,
-  ActionButtonIconSmall
+  ActionButtonIconSmall,
+  ActionButtonXsmall
 } from '../../elements/ActionButton';
-import { getXfinityTheme } from '../../Styles/createTheme';
-
+import icApps from '../../assets/images/ic_apps_white_32.png';
+import icOverflowVertical from '../../assets/images/ic_overflow_vertical_white_32.png';
+import icPlay from '../../assets/images/ic_play_white_32.png';
+import icAvatar from '../../assets/images/ic_avatar_white_32.png';
+import icMic from '../../assets/images/ic_mic_white_32.png';
+import CollectionDocItem from '../CollectionDocItem';
+import CollectionHeader from '../CollectionHeader';
+import CollectionLabel from '../CollectionLabel';
 export default {
   title: 'Collections/Buttons',
   parameters: {
@@ -24,133 +27,180 @@ export default {
   }
 };
 
-const theme = getXfinityTheme();
-const style = {
-  space: theme.spacing(4)
+const styles = theme => {
+  return {
+    space: theme.spacing(4)
+  };
 };
 
 export const Basic = () => {
-  class Basic extends lng.Component {
+  class Basic extends withStyles(lng.Component, styles) {
     static _template() {
       return {
-        ButtonCollection: {
+        Buttons: {
           Column: {
             type: Column,
-            itemSpacing: style.space,
+            itemSpacing: this.styles.space,
             items: [
               {
-                type: CollectionGroup,
-                title: 'Large Standard',
+                type: CollectionHeader,
+                url:
+                  'https://etwiki.sys.comcast.net/display/XDSS/Action+buttons'
+              },
+              {
+                type: CollectionLabel,
+                title: 'Action Button Large'
+              },
+              {
+                type: CollectionDocItem,
                 items: [
                   {
-                    h: ActionButtonLarge.styles.h,
-                    type: Row,
-                    itemSpacing: style.space,
-                    items: [
-                      {
-                        type: ActionButtonLarge,
-                        backgroundType: 'fill',
-                        icon: icApps,
-                        title: 'Button'
-                      },
-                      {
-                        type: ActionButtonLarge,
-                        icon: icApps,
-                        title: 'Button'
-                      }
-                    ]
+                    type: ActionButtonLarge,
+                    backgroundType: 'fill',
+                    icon: icApps,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonLarge,
+                    icon: icApps,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonLarge,
+                    icon: icApps,
+                    title: 'Button (focus)'
                   }
                 ]
               },
               {
-                type: CollectionGroup,
-                title: 'Medium Standard',
+                type: CollectionLabel,
+                title: 'Action Button Medium'
+              },
+              {
+                type: CollectionDocItem,
                 items: [
                   {
-                    h: ActionButtonMedium.styles.h,
-                    type: Row,
-                    itemSpacing: style.space,
-                    items: [
-                      {
-                        type: ActionButtonMedium,
-                        backgroundType: 'stroke',
-                        icon: icPlay,
-                        title: 'Button'
-                      },
-                      {
-                        type: ActionButtonMedium,
-                        icon: icPlay,
-                        title: 'Button'
-                      }
-                    ]
+                    type: ActionButtonMedium,
+                    backgroundType: 'stroke',
+                    icon: icPlay,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonMedium,
+                    icon: icPlay,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonMedium,
+                    icon: icPlay,
+                    title: 'Button (focus)'
                   }
                 ]
               },
               {
-                type: CollectionGroup,
-                title: 'Medium Icon',
+                type: CollectionLabel,
+                title: 'Action Button Icon Medium'
+              },
+              {
+                type: CollectionDocItem,
                 items: [
                   {
-                    h: ActionButtonIconMedium.styles.h,
-                    type: Row,
-                    itemSpacing: style.space,
-                    items: [
-                      {
-                        type: ActionButtonIconMedium,
-                        backgroundType: 'stroke',
-                        icon: icOverflowVertical
-                      },
-                      {
-                        type: ActionButtonIconMedium,
-                        icon: icOverflowVertical
-                      }
-                    ]
+                    type: ActionButtonIconMedium,
+                    backgroundType: 'stroke',
+                    icon: icOverflowVertical,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonIconMedium,
+                    icon: icOverflowVertical,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonIconMedium,
+                    icon: icOverflowVertical,
+                    title: 'Button (focus)'
                   }
                 ]
               },
               {
-                type: CollectionGroup,
-                title: 'Small Standard',
+                type: CollectionLabel,
+                title: 'Action Button Small'
+              },
+              {
+                type: CollectionDocItem,
                 items: [
                   {
-                    h: ActionButtonSmall.styles.h,
-                    type: Row,
-                    itemSpacing: style.space,
-                    items: [
-                      {
-                        type: ActionButtonSmall,
-                        backgroundType: 'stroke',
-                        icon: icPlay,
-                        title: 'Button'
-                      },
-                      {
-                        type: ActionButtonSmall,
-                        icon: icPlay,
-                        title: 'Button'
-                      }
-                    ]
+                    type: ActionButtonSmall,
+                    backgroundType: 'stroke',
+                    icon: icPlay,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonSmall,
+                    icon: icPlay,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonSmall,
+                    icon: icPlay,
+                    title: 'Button (focus)'
                   }
                 ]
               },
               {
-                type: CollectionGroup,
-                title: 'Small Icon',
+                type: CollectionLabel,
+                title: 'Action Button Icon Small'
+              },
+              {
+                type: CollectionDocItem,
                 items: [
                   {
-                    h: 60,
-                    type: Row,
-                    itemSpacing: style.space,
-                    items: [
-                      {
-                        type: ActionButtonIconSmall,
-                        backgroundType: 'stroke',
-                        icon: icOverflowVertical
-                      },
-                      {
-                        type: ActionButtonIconSmall,
-                        icon: icOverflowVertical
-                      }
-                    ]
+                    type: ActionButtonIconSmall,
+                    backgroundType: 'stroke',
+                    icon: icOverflowVertical,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonIconSmall,
+                    icon: icOverflowVertical,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonIconSmall,
+                    icon: icOverflowVertical,
+                    title: 'Button (focus)'
+                  }
+                ]
+              },
+              {
+                type: CollectionLabel,
+                title: 'Action Button Xsmall'
+              },
+              {
+                type: CollectionDocItem,
+                items: [
+                  {
+                    type: ActionButtonXsmall,
+                    backgroundType: 'stroke',
+                    icon: icAvatar,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonXsmall,
+                    icon: icAvatar,
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonXsmall,
+                    icon: icMic,
+                    backgroundType: 'ghost',
+                    title: 'Button'
+                  },
+                  {
+                    type: ActionButtonXsmall,
+                    icon: icMic,
+                    backgroundType: 'ghost',
+                    title: 'Button (focus)'
                   }
                 ]
               }
@@ -161,7 +211,7 @@ export const Basic = () => {
     }
 
     _getFocused() {
-      return this.tag('ButtonCollection.Column');
+      return this.tag('Buttons.Column');
     }
   }
 
