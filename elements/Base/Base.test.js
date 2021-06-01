7891d104838db52709e305705b4be601c9df712b
Gallo, Anthony (Business Partner) <anthony.gallo@thinkcompany.com>
2021-06-01 16:46:07 -0400

    feat: add Base component (#507)
    * feat: add Base component
    
    * feat: add default methods to the base component
    
    * Trigger Concourse
    
    * fix: kick Concourse script was missing no-verify
    
    Co-authored-by: Rautenberg, Emily <Emily_Rautenberg@comcast.com>

---
+++ elements/Base/Base.test.js
@@ -0,0 +1,21 @@
+import TestUtils from '../../test/lightning-test-utils';
+import Base from '.';
+
+const createComponent = TestUtils.makeCreateComponent(Base);
+
+describe('Base', () => {
+  let component, testRenderer;
+
+  beforeEach(() => {
+    [component, testRenderer] = createComponent();
+  });
+  afterEach(() => {
+    component = null;
+    testRenderer = null;
+  });
+
+  it('renders', () => {
+    const tree = testRenderer.toJSON(2);
+    expect(tree).toMatchSnapshot();
+  });
+});
