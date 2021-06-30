1a7847b69e785ffae64c70ba4347e483481b69c6
Richards, David (Contractor) <David_Richards3@comcast.com>
2021-06-30 12:24:10 -0400

    docs(Card): add docs and story args (#540)
    * docs(card): add card doc
    
    * docs(card): add args for card stories
    
    * docs(card): adjust docs and stories based on feedback
    
    * docs(card): move progress bar link

--- patterns/Card/Card.stories.js
+++ patterns/Card/Card.stories.js
@@ -27,9 +27,9 @@ export const Basic = args =>
       return {
         Card: {
           type: BasicCard,
-          title: 'Title',
-          description: 'Short description',
-          logo: circle,
+          title: args.title,
+          description: args.description,
+          logo: args.showLogo ? circle : null,
           logoWidth: 1920,
           logoHeight: 1080
         }
@@ -37,16 +37,24 @@ export const Basic = args =>
     }
 
     _getFocused() {
-      return this.tag('Card');
+      if (args.focused) {
+        return this.tag('Card');
+      }
     }
   };
-// Basic.args = {};
-// Basic.argTypes = {};
-// Basic.parameters = {
-//   argActions: {}
-// };
+Basic.args = {
+  title: 'Title',
+  description: 'Short Description',
+  showLogo: true
+};
+Basic.argTypes = {
+  title: { content: 'text' },
+  description: { content: 'text' },
+  showLogo: { content: 'boolean' },
+  focused: { control: 'boolean' }
+};
 
-export const Horziontal = args =>
+export const Horizontal = args =>
   class Horizontal extends lng.Component {
     static _template() {
       return {
@@ -55,17 +63,19 @@ export const Horziontal = args =>
           src: pets,
           artHeight: 1080,
           artWidth: 1920,
-          title: 'Title',
-          description: 'Short description',
-          data: [
-            '86%',
-            {
-              icon:
-                'http://myriad.merlin.comcast.com/select/logo?entityId=8527084350383982239&width=32&height=&ratio=1x1&trim=false',
-              title: 'Rotten Tomatoes rating'
-            }
-          ],
-          logo: circle,
+          title: args.title,
+          description: args.description,
+          data: args.showData
+            ? [
+                '86%',
+                {
+                  icon:
+                    'http://myriad.merlin.comcast.com/select/logo?entityId=8527084350383982239&width=32&height=&ratio=1x1&trim=false',
+                  title: 'Rotten Tomatoes rating'
+                }
+              ]
+            : null,
+          logo: args.showLogo ? circle : null,
           logoWidth: 1920,
           logoHeight: 1080
         }
@@ -73,16 +83,26 @@ export const Horziontal = args =>
     }
 
     _getFocused() {
-      return this.tag('Card');
+      if (args.focused) {
+        return this.tag('Card');
+      }
     }
   };
-// Horizontal.args = {};
-// Horizontal.argTypes = {};
-// Horizontal.parameters = {
-//   argActions: {}
-// };
+Horizontal.args = {
+  title: 'Title',
+  description: 'Short Description',
+  showData: true,
+  showLogo: true
+};
+Horizontal.argTypes = {
+  title: { content: 'text' },
+  description: { content: 'text' },
+  showData: { content: 'boolean' },
+  showLogo: { content: 'boolean' },
+  focused: { control: 'boolean' }
+};
 
-export const HorziontalLarge = args =>
+export const HorizontalLarge = args =>
   class HorizontalLarge extends lng.Component {
     static _template() {
       return {
@@ -93,15 +113,17 @@ export const HorziontalLarge = args =>
           artWidth: 1920,
           title: 'Title',
           description: 'Short description',
-          data: [
-            '86%',
-            {
-              icon:
-                'http://myriad.merlin.comcast.com/select/logo?entityId=8527084350383982239&width=32&height=&ratio=1x1&trim=false',
-              title: 'Rotten Tomatoes rating'
-            }
-          ],
-          logo: circle,
+          data: args.showData
+            ? [
+                '86%',
+                {
+                  icon:
+                    'http://myriad.merlin.comcast.com/select/logo?entityId=8527084350383982239&width=32&height=&ratio=1x1&trim=false',
+                  title: 'Rotten Tomatoes rating'
+                }
+              ]
+            : null,
+          logo: args.showLogo ? circle : null,
           logoWidth: 1920,
           logoHeight: 1080
         }
@@ -109,14 +131,24 @@ export const HorziontalLarge = args =>
     }
 
     _getFocused() {
-      return this.tag('Card');
+      if (args.focused) {
+        return this.tag('Card');
+      }
     }
   };
-// HorizontalLarge.args = {};
-// HorizontalLarge.argTypes = {};
-// HorizontalLarge.parameters = {
-//   argActions: {}
-// };
+HorizontalLarge.args = {
+  title: 'Title',
+  description: 'Short Description',
+  showData: true,
+  showLogo: true
+};
+HorizontalLarge.argTypes = {
+  title: { content: 'text' },
+  description: { content: 'text' },
+  showData: { content: 'boolean' },
+  showLogo: { content: 'boolean' },
+  focused: { control: 'boolean' }
+};
 
 export const Vertical = args =>
   class Vertical extends lng.Component {
@@ -127,21 +159,27 @@ export const Vertical = args =>
           src: pets,
           artHeight: 1080,
           artWidth: 1920,
-          title: 'Title',
-          description: 'Short description'
+          title: args.title,
+          description: args.description
         }
       };
     }
 
     _getFocused() {
-      return this.tag('Card');
+      if (args.focused) {
+        return this.tag('Card');
+      }
     }
   };
-// Vertical.args = {};
-// Vertical.argTypes = {};
-// Vertical.parameters = {
-//   argActions: {}
-// };
+Vertical.args = {
+  title: 'Title',
+  description: 'Short Description'
+};
+Vertical.argTypes = {
+  title: { content: 'text' },
+  description: { content: 'text' },
+  focused: { control: 'boolean' }
+};
 
 export const VerticalLarge = args =>
   class VerticalLarge extends lng.Component {
@@ -152,17 +190,19 @@ export const VerticalLarge = args =>
           src: pets,
           artHeight: 1080,
           artWidth: 1920,
-          title: 'Title',
-          description: 'Short description',
-          data: [
-            '86%',
-            {
-              icon:
-                'http://myriad.merlin.comcast.com/select/logo?entityId=8527084350383982239&width=32&height=&ratio=1x1&trim=false',
-              title: 'Rotten Tomatoes rating'
-            }
-          ],
-          logo: circle,
+          title: args.title,
+          description: args.description,
+          data: args.showData
+            ? [
+                '86%',
+                {
+                  icon:
+                    'http://myriad.merlin.comcast.com/select/logo?entityId=8527084350383982239&width=32&height=&ratio=1x1&trim=false',
+                  title: 'Rotten Tomatoes rating'
+                }
+              ]
+            : null,
+          logo: args.showLogo ? circle : null,
           logoWidth: 1920,
           logoHeight: 1080
         }
@@ -170,14 +210,24 @@ export const VerticalLarge = args =>
     }
 
     _getFocused() {
-      return this.tag('Card');
+      if (args.focused) {
+        return this.tag('Card');
+      }
     }
   };
-// VerticalLarge.args = {};
-// VerticalLarge.argTypes = {};
-// VerticalLarge.parameters = {
-//   argActions: {}
-// };
+VerticalLarge.args = {
+  title: 'Title',
+  description: 'Short Description',
+  showData: true,
+  showLogo: true
+};
+VerticalLarge.argTypes = {
+  title: { content: 'text' },
+  description: { content: 'text' },
+  showData: { content: 'boolean' },
+  showLogo: { content: 'boolean' },
+  focused: { control: 'boolean' }
+};
 
 export const VerticalDynamic = args =>
   class VerticalDynamic extends lng.Component {
@@ -188,10 +238,10 @@ export const VerticalDynamic = args =>
           src: pets,
           artWidth: 1920,
           artHeight: 1080,
-          title: 'Title',
-          description: 'Short description',
-          action: 'Watch on Netflix',
-          logo: circle,
+          title: args.title,
+          description: args.description,
+          action: args.action,
+          logo: args.showLogo ? circle : null,
           logoWidth: 1920,
           logoHeight: 1080
         }
@@ -199,11 +249,21 @@ export const VerticalDynamic = args =>
     }
 
     _getFocused() {
-      return this.tag('Card');
+      if (args.focused) {
+        return this.tag('Card');
+      }
     }
   };
-// VerticalDynamic.args = {};
-// VerticalDynamic.argTypes = {};
-// VerticalDynamic.parameters = {
-//   argActions: {}
-// };
+VerticalDynamic.args = {
+  title: 'Title',
+  description: 'Short Description',
+  action: 'Watch on Netflix',
+  showLogo: true
+};
+VerticalDynamic.argTypes = {
+  title: { content: 'text' },
+  description: { content: 'text' },
+  action: { content: 'text' },
+  showLogo: { content: 'boolean' },
+  focused: { control: 'boolean' }
+};
