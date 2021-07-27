630bf8c4c03e7776b46bf48775876206d2bbfdb7
Richards, David (Contractor) <David_Richards3@comcast.com>
2021-07-27 12:09:06 -0400

    refactor(ProgressBar): refactor to extend off of Base and pull out styles (#582)
    * refactor(progressBar): refactor to extend off of base and pull out styles
    
    * refactor(progressBar): update tests
    
    * refactor(progressBar): add setters and update tests
    
    * test(ProgressBar): update Column snapshot
    
    * refactor(progressBar): fix color setters
    
    * test(progressBar): update column snapshot
    
    Co-authored-by: eraute200 <emily_rautenberg@comcast.com>

---
+++ elements/ProgressBar/ProgressBar.styles.js
@@ -0,0 +1,7 @@
+export default theme => ({
+  h: 8,
+  w: 410,
+  radius: theme.border.radius.xsmall,
+  barColor: theme.palette.grey[20],
+  progressColor: theme.palette.getHexColor(theme.palette.grey[0], 96)
+});
