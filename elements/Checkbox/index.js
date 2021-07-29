a0ca094e9e6467e23ccf816a2da12ddbc9d94e42
David Richards, Jr <dbarichardsjr@gmail.com>
2021-07-29 16:08:08 -0400

    refactor(Checkbox): refactor to extend Base and pull out styles (#600)

--- elements/Checkbox/index.js
+++ elements/Checkbox/index.js
@@ -1,19 +1,9 @@
 import lng from '@lightningjs/core';
 import withStyles from '../../mixins/withStyles';
+import Base from '../Base';
+import styles from './Checkbox.styles';
 
-const Check =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAOCAYAAAAvxDzwAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAADgAAAACYjX/eAAABKElEQVQ4EbXSO04DMRAGYHtlWaJYpYMcgSBxAUhFi1KkoUrBJVLmENR0XIDUFOQSCTT0SJEziLXzFOzwD3lIWbLahIAby/b42xnPavUHw3t/OJvxPaj3aF9vjqkOszqDVdL7gCGEo+mUH5n5RGvds1Zd/NoTbDDwPecSxtyVTP8Hg36Fr9S31ReZPeVmhoMANHUuXBehhRiAKIp0E4+KN+VbomEtDwVWRgPQTa4sGxDHcX9jPDJsSQnO+RFRUs0GCYaz59wysxdkDfRmfiF5Iwqny5hdsdV/KGUDvQPUUEq/GmPOjfkcTybcUYqPC8tcZLACZQ3UEPk2/vpLLF8Af+yCibEGygbQA5T8gPn7LbfNTO7K+AHKJhGX0jS00Xlrra7ndlOCM+MLVJf04NpHPKcAAAAASUVORK5CYII=';
-
-export const styles = theme => ({
-  w: 32,
-  h: 32,
-  radius: 4,
-  background: { color: theme.palette.background.default },
-  stroke: { color: theme.palette.grey[5], width: 4 },
-  check: { w: 20, h: 14, src: Check }
-});
-
-class Checkbox extends lng.Component {
+export default class Checkbox extends withStyles(Base, styles) {
   static _template() {
     return {
       w: this.styles.w,
@@ -39,6 +29,10 @@ class Checkbox extends lng.Component {
     };
   }
 
+  static get tags() {
+    return ['Check'];
+  }
+
   _init() {
     this._update();
   }
@@ -57,10 +51,4 @@ class Checkbox extends lng.Component {
     const { checked } = this;
     this._Check.smooth = { alpha: checked ? 1 : 0 };
   }
-
-  get _Check() {
-    return this.tag('Check');
-  }
 }
-
-export default withStyles(Checkbox, styles);
