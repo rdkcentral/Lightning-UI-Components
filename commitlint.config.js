33046478324b2dcb5729499ea4e9288b0a5b10e7
Josh Howenstine <Joshhowenstine@gmail.com>
2021-08-04 11:46:32 -0700

    fix(Husky): never throw an error on footer-max-line-length (#603)
    Co-authored-by: Rautenberg, Emily <Emily_Rautenberg@comcast.com>
    Co-authored-by: Anthony <anthony.gallo@thinkcompany.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,9 @@
+const ERROR = 2;
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-case': [0]
+    'scope-case': [0],
+    'footer-max-line-length': [ERROR, 'never']
   }
 };
