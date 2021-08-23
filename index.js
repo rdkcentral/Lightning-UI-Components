62f7c8a4a712961a046e0bc25fd33432ab8f87db
Rautenberg, Emily <Emily_Rautenberg@comcast.com>
2021-08-23 09:21:35 -0400

    chore: add scripts to automate updating next and beta branches (#635)
    * chore: add scripts to automate updating next and beta branches
    
    * chore: remove outdated deprectation notice (no longer applicable)
    
    Co-authored-by: Gallo, Anthony (Business Partner) <anthony.gallo@thinkcompany.com>

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/**
- *
- * DEPRECATION NOTICE
- *
- * This file will be removed in a future version, removing all components from the default export.
- * Components should instead be imported from the specific folder:
- *
- * import { ActionButton } from '@lightning/ui/elements;
- *
- */
-
 export * from './Styles/index.js';
 export * from './elements';
 export * from './layout';
