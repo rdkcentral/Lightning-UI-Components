236a5bf8dbb050f249232975dbbf38730bafa136
Tim Hojnoski <timhojnoski@gmail.com>
2021-09-10 14:00:33 -0400

    feat(mixins): add withTags mixin

---
+++ mixins/withTags/withTags.stories.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2020 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import lng from '@lightningjs/core';
+import mdx from './withTags.mdx';
+import withTags from '.';
+
+export default {
+  title: 'Mixins/withTags',
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  }
+};
+
+export const Basic = args =>
+  class Basic extends withTags(lng.Component) {
+    static get tags() {
+      return ['Text'];
+    }
+
+    static _template() {
+      return {
+        x: 50,
+        y: 50,
+        Text: {
+          text: 'This has a tag'
+        }
+      };
+    }
+  };
+
+Basic.args = {};
+Basic.argTypes = {};
