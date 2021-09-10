482af35fb643d6cc11bb494701c3d3e876a88eef
Tim Hojnoski <timhojnoski@gmail.com>
2021-09-10 09:56:15 -0400

    feat(Base): add Base component

---
+++ components/Base/Base.stories.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2020 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import mdx from './Base.mdx';
+
+export default {
+  title: 'Base',
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  }
+};
+
+export const Basic = args =>
+  class Basic extends lng.Component {
+    static _template() {
+      return {
+        Text: {
+          text: {
+            text: 'See Docs'
+          }
+        }
+      };
+    }
+
+    _getFocused() {
+      if (args.focused) {
+        return this.tag('Text');
+      }
+    }
+  };
---
+++ components/Base/Base.test.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2020 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import TestUtils from '../lightning-test-utils';
+import Base from '.';
+
+const createComponent = TestUtils.makeCreateComponent(Base);
+
+describe('Base', () => {
+  let component, testRenderer;
+
+  beforeEach(() => {
+    [component, testRenderer] = createComponent();
+  });
+  afterEach(() => {
+    component = null;
+    testRenderer = null;
+  });
+
+  it('renders', () => {
+    const tree = testRenderer.toJSON(2);
+    expect(tree).toMatchSnapshot();
+  });
+});
