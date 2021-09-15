82155271f8f962c147418080862a7b62a308a38e
Anthony <anthony.gallo@thinkcompany.com>
2021-09-15 16:21:04 -0400

    chore: add index files to elements and layout dirs

---
+++ elements/index.js
@@ -0,0 +1,6 @@
+export { default as Announcer } from './Announcer';
+export { default as Base } from './Base';
+export { default as Button } from './Button';
+export { default as Icon } from './Icon';
+export { default as Keyboard } from './Keyboard';
+export { default as MarqueeText } from './MarqueeText';
