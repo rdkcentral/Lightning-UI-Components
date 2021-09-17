39b39d1abbfa1882d10942ed57f127b568e0341a
Tim Hojnoski <timhojnoski@gmail.com>
2021-09-17 16:50:59 -0400

    chore(jest): remove comment

--- jest.config.js
+++ jest.config.js
@@ -88,7 +88,7 @@ module.exports = {
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: ['<rootDir>/mixins/withVibrant'],
+  // modulePathIgnorePatterns: [],
 
   // Activates notifications for test results
   // notify: false,
