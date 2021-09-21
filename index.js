6d63031d694294921357bf054d52d0791b5face8
Anthony Gallo <anthony9187@users.noreply.github.com>
2021-09-21 15:33:32 -0400

    chore: update exports (#57)
    * fix: export keyboard formats  from elements
    
    * chore: add withHandleKey and withTags to mixin exports
    
    * chore: simplify root export file to export all from each component dir

--- index.js
+++ index.js
@@ -15,13 +15,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-export * from './components/Styles/index.js';
-export { default as Announcer } from './components/Announcer/index.js';
-export { default as Button } from './components/Button/index.js';
-export { default as Column } from './components/Column/index.js';
-export { default as FocusManager } from './components/FocusManager/index.js';
-export { default as Icon } from './components/Icon/index.js';
-export { default as MarqueeText } from './components/MarqueeText/index.js';
-export { default as Row } from './components/Row/index.js';
-export { default as Base } from './components/Base';
-export { default as withHandleKey } from './mixins/withHandleKey';
+export * from './elements';
+export * from './layouts';
+export * from './mixins';
+export * from './Styles';
--- mixins/index.js
+++ mixins/index.js
@@ -16,8 +16,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+export { default as withHandleKey } from './withHandleKey';
 export { default as withSelections } from '/withSelections';
 export { default as withStyles } from './withStyles';
+export { default as withTags } from './withTags';
 export { default as withTheme } from './withTheme';
 export { default as withTransitions } from './withTransitions';
 export { default as withUpdates } from './withUpdates';
