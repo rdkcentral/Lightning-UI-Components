3fc96bb59c1eca047b45da331156728a4bfd1b75
David Richards, Jr <dbarichardsjr@gmail.com>
2021-09-27 16:14:12 -0400

    refactor(OverlayDataItem): refactor to extend Base (#672)

---
+++ patterns/OverlayDataItem/OverlayDataItem.styles.js
@@ -0,0 +1,12 @@
+export default theme => ({
+  image: {
+    alpha: 0.2,
+    justify: 'right',
+    size: 160,
+    margin: theme.spacing(2)
+  },
+  color: {
+    alpha: 0.5,
+    radius: theme.border.radius.medium
+  }
+});
