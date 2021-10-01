0442ad0c5d56c2fba8976dfe469b8f9085871291
Tim Hojnoski <timhojnoski@gmail.com>
2021-10-01 14:27:47 -0400

    fix(exports): correct layout export path

--- index.js
+++ index.js
@@ -16,6 +16,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 export * from './elements';
-export * from './layouts';
+export * from './layout';
 export * from './mixins';
 export * from './Styles';
