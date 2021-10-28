0680b10c5a347d5bdd52622a8b0634f3f80fe3fd
Simek, Chelsea <Chelsea_Simek@comcast.com>
2021-10-28 10:06:54 -0400

    feat(theming): base theme (#722)
    * feat(theme): base theme raidus
    
    * feat(theming): base theme type scale
    
    * feat(theming): base colors
    
    * feat(theming): fix typo

--- themes/base/typography.js
+++ themes/base/typography.js
@@ -1,97 +1,85 @@
 export default {
   display1: {
     fontFace: 'Arial',
-    fontSize: 56,
-    fontWeight: 700,
-    letterSpacing: -0.4,
-    lineHeight: 72,
+    fontSize: 75,
+    fontWeight: 500,
     verticalAlign: 'middle'
   },
   display2: {
     fontFace: 'Arial',
-    fontSize: 48,
-    fontWeight: 700,
-    letterSpacing: -0.4,
-    lineHeight: 64,
+    fontSize: 50,
+    fontWeight: 500,
     verticalAlign: 'middle'
   },
   headline1: {
     fontFace: 'Arial',
-    fontSize: 36,
-    fontWeight: 700,
-    letterSpacing: -0.2,
+    fontSize: 35,
+    fontWeight: 500,
     lineHeight: 48,
     verticalAlign: 'middle'
   },
   headline2: {
     fontFace: 'Arial',
-    fontSize: 32,
-    fontWeight: 700,
-    letterSpacing: -0.2,
+    fontSize: 30,
+    fontWeight: 500,
     lineHeight: 40,
     verticalAlign: 'middle'
   },
   headline3: {
     fontFace: 'Arial',
-    fontSize: 28,
-    fontWeight: 700,
-    letterSpacing: -0.2,
+    fontSize: 25,
+    fontWeight: 500,
     lineHeight: 36,
     verticalAlign: 'middle'
   },
   body1: {
     fontFace: 'Arial',
-    fontSize: 28,
-    fontWeight: 500,
-    letterSpacing: 0,
+    fontSize: 25,
+    fontWeight: 300,
     lineHeight: 40,
     verticalAlign: 'middle'
   },
   body2: {
     fontFace: 'Arial',
-    fontSize: 24,
-    fontWeight: 500,
-    letterSpacing: 0,
+    fontSize: 22,
+    fontWeight: 300,
     lineHeight: 32,
     verticalAlign: 'middle'
   },
   body3: {
     fontFace: 'Arial',
-    fontSize: 24,
-    fontWeight: 500,
-    letterSpacing: 0,
+    fontSize: 20,
+    fontWeight: 300,
     lineHeight: 32,
     verticalAlign: 'middle'
   },
   button1: {
     fontFace: 'Arial',
-    fontSize: 28,
-    fontWeight: 700,
+    fontSize: 25,
+    fontWeight: 500,
     letterSpacing: -0.2,
     lineHeight: 32,
     verticalAlign: 'middle'
   },
   button2: {
     fontFace: 'Arial',
-    fontSize: 24,
-    fontWeight: 700,
-    letterSpacing: -0.2,
+    fontSize: 20,
+    fontWeight: 500,
     lineHeight: 32,
     verticalAlign: 'middle'
   },
   callout1: {
     fontFace: 'Arial',
-    fontSize: 24,
-    fontWeight: 700,
-    letterSpacing: 2,
+    fontSize: 20,
+    fontWeight: 500,
+    letterSpacing: 1,
     lineHeight: 32,
     verticalAlign: 'middle'
   },
   caption: {
     fontFace: 'Arial',
-    fontSize: 20,
-    fontWeight: 700,
-    letterSpacing: 0,
+    fontSize: 15,
+    fontWeight: 500,
     lineHeight: 24,
     verticalAlign: 'middle'
   }
