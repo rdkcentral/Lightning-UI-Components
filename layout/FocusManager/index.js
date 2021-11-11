972025c97289f921b770a256680a6ff209905e83
David Richards, Jr <dbarichardsjr@gmail.com>
2021-11-11 10:16:28 -0500

    fix(FocusManager): default x and y values and null check in getIndexOfItemNear (#734)

--- layout/FocusManager/index.js
+++ layout/FocusManager/index.js
@@ -180,7 +180,10 @@ export default class FocusManager extends lng.Component {
     // Get all item center points from selected
     const selectedCoordArray = selected.items
       .map((item, index) => {
-        const [x, y] = item.core.getAbsoluteCoords(0, 0);
+        let [x, y] = [0, 0];
+        if (item.core) {
+          [x, y] = item.core.getAbsoluteCoords(0, 0);
+        }
         return {
           index,
           distance: !item.skipFocus
