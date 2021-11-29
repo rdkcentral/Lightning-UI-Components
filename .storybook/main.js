f53b6dff076d1b3576ae57a16fa394457a2f37e5
Howenstine, Joshua (Contractor) <Joshua_Howenstine@comcast.com>
2021-11-29 14:25:04 -0800

    fix(FocusManager): reset transitions in Focus manager when items is set (#744)
    * fix(FocusManager): reset transitions in Focus manager when items is set
    
    * fix(FocusManager): reset targetValue to 0  for transitions on new items
    
    * fix(Column): wait for transition to finish before firing onScreenEffect
    
    * fix(Column): resets value of transition('y').targetValue when patch used
    
    * fix(Column): onScreenEffect fix. Remove updateTargetValue duplication
    
    * fix(Column): change _resetTransitionTarget to _updateTransitionTarget
    
    * fix(Column): this._transitionListener binding
    
    * fix: merge develop and fix issue with storybook glob pattern
    
    Co-authored-by: Rautenberg, Emily <Emily_Rautenberg@comcast.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,21 +6,21 @@ const stories =
         '../docs/Introduction.stories.mdx',
         '../docs/Readme.stories.mdx',
         '../docs/Contributing.stories.mdx',
-        '../collections/(**/)?*.stories.{js,mdx}',
+        '../collections/**/*.stories.{js,mdx}',
         '../elements/**/*.stories.{js,mdx}',
         '../layout/**/*.stories.{js,mdx}',
-        '../patterns/(**/)?*.stories.{js,mdx}',
-        '../mixins/(**/)?*.stories.{js,mdx}'
+        '../patterns/**/*.stories.{js,mdx}',
+        '../mixins/**/*.stories.{js,mdx}'
       ]
     : [
         '../docs/Introduction.stories.mdx',
         '../docs/Readme.stories.mdx',
         '../docs/Contributing.stories.mdx',
-        '../collections/(**/)?*.stories.{js,mdx}',
+        '../collections/**/*.stories.{js,mdx}',
         '../elements/**/*.stories.{js,mdx}',
         '../layout/**/*.stories.{js,mdx}',
         '../patterns/**/!(ListItemBase).stories.{js,mdx}',
-        '../mixins/(**/)?*.stories.{js,mdx}'
+        '../mixins/**/*.stories.{js,mdx}'
       ];
 
 module.exports = {
