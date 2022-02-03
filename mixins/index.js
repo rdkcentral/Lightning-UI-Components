ea9292cf7b4ae4bc807e12d6758ca4a52e8fdc3a
Ischenko, Denis (Contractor) <Denis_Ischenko@comcast.com>
2022-02-03 21:58:59 +0200

    feat(SoftFocus): implemented soft focus marker mixin (#829)
    * feat(SoftFocus): implemented soft focus marker mixin
    
    * feat(SoftFocus): implemented soft focus marker mixin
    
    * feat(SoftFocus): implemented soft focus marker mixin
    
    Co-authored-by: dischenko <dischenko@productengine.com>

--- mixins/index.js
+++ mixins/index.js
@@ -13,3 +13,4 @@ export { default as withTags } from './withTags';
 export { default as withTransitions } from './withTransitions';
 export { default as withUpdates } from './withUpdates';
 export { default as withVibrant } from './withVibrant';
+export { default as withSoftFocus } from './withSoftFocus';
