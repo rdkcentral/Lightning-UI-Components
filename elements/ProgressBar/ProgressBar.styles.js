14315a2a6157f886e704287921dc172e89c24aa1
Emily Rautenberg <erautenb@gmail.com>
2022-04-26 11:53:53 -0400

    fix(ProgressBar): update colors (#923)

--- elements/ProgressBar/ProgressBar.styles.js
+++ elements/ProgressBar/ProgressBar.styles.js
@@ -2,6 +2,6 @@ export default theme => ({
   h: 8,
   w: 410,
   radius: theme.border.radius.xsmall,
-  barColor: theme.palette.grey[20],
-  progressColor: theme.palette.getHexColor(theme.palette.grey[0], 96)
+  barColor: theme.palette.getHexColor(theme.palette.grey[5], 15),
+  progressColor: theme.palette.getHexColor(theme.palette.grey[5], 100)
 });
