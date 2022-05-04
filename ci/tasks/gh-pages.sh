d6d853ebde14e92ba709d6f7f7522b17dda86509
Emily Rautenberg <erautenb@gmail.com>
2022-05-04 10:03:33 -0400

    chore: Deploy docs to CloudFoundry (#933)
    * chore: Extracting docs build step into a separate task
    
    * chore: Adding a manifest file for CloudFoundry deployments
    
    * chore: Replacing cloudfoundry task with cf resource type
    
    * chore: fixing repo references

--- ci/tasks/gh-pages.sh
+++ ci/tasks/gh-pages.sh
@@ -4,9 +4,6 @@ ssh-keyscan -H $HOST >> ~/.ssh/known_hosts
 eval $(ssh-agent -s)
 ssh-add <(echo "$PRIVATE_KEY")
 
-npm install
-npm run build
-
-git config user.name $USERNAME 
+git config user.name $USERNAME
 git config user.email $EMAIL
 npm run gh-pages
