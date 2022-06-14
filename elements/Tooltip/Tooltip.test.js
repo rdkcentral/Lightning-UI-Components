d19eba68bdedaec2aa158e41d6869ae4d3235450
Hojnoski, Tim (Contractor) <Tim_Hojnoski@comcast.com>
2022-06-14 09:29:54 -0400

    feat(Tooltip): refactor for theming (#950)
    * feat(Tooltip): first pass setting up theming WIP
    
    * feat(Tooltip): themed bubble and text
    
    * feat(textures): add down arrow texture
    
    * feat(Tooltip): add down pointer
    
    * feat(Tooltip): render point
    
    * chore(textures): remove unused texture
    
    * feat(Tooltip): update color with theme switcher
    
    * docs(Tooltip): update only tooltip with props
    
    * feat(Tooltip): split into more methods
    
    * docs(Tooltip): update docs after theme refactor
    
    * feat(Tooltip): remove unneeded y adjustment
    
    * feat(Tooltip): add TS definition
    
    * fix(Tooltip): remove redundant method
    
    * chore(Tooltip): use if instead of ternary
    
    * test(Tooltip): update tests post theme refactor
    
    * chore(Tooltip): remove repeated style
    
    * test(Tooltip): dimensions tests
    
    * docs(Tooltip): add style properties
    
    * feat(texture): first draft of Blurb texture
    
    * feat(Bubble): refine and rename Blurb texture
    
    * feat(Tooltip): use Bubble texture for shape
    
    * test(Tooltip): update snapshot with new texture
    
    * docs(Tooltip): favor displaying just tooltip in stories
    
    * feat(Tooltip): update texture to better match designs
    
    * test(Tooltip): update for different element dimensions
    
    * feat(textures): remove unused fields
    
    * fix(texture): add more width to canvas to prevent clipping
    
    * docs(Tooltip): default focused in stories
    
    * feat(Tooltip): prevent Tooltip from being removed on fade animation
    
    * feat(Tooltip): use themed animation props
    
    * feat(Tooltip): get transition props from style file
    
    * test(Tooltip): update for themed transitions
    
    * test(Tooltip): rename helper function
    
    * feat(Tooltip): updated TS definition

--- elements/Tooltip/Tooltip.test.js
+++ elements/Tooltip/Tooltip.test.js
@@ -1,15 +1,30 @@
-'use strict';
-
 import TestUtils from '../../test/lightning-test-utils';
 import Tooltip from '.';
-import Button from '../Button';
 
 const createTooltip = TestUtils.makeCreateComponent(
   Tooltip,
   {},
   { focused: false }
 );
-const createButton = TestUtils.makeCreateComponent(Button);
+
+const completeAnimation = (element, transitionProperties = []) => {
+  const props = Array.isArray(transitionProperties)
+    ? transitionProperties
+    : [transitionProperties];
+
+  const transitions = props.map(prop => {
+    return new Promise(resolve => {
+      element._getTransition(prop).once('finish', () => {
+        resolve();
+      });
+    });
+  });
+
+  return Promise.all(transitions);
+};
+
+jest.useFakeTimers();
+jest.spyOn(global, 'setTimeout');
 
 describe('Tooltip', () => {
   let tooltip, testRenderer;
@@ -17,229 +32,126 @@ describe('Tooltip', () => {
   beforeEach(() => {
     [tooltip, testRenderer] = createTooltip({ title: 'Tooltip' });
   });
-  afterEach(() => {
-    tooltip = null;
-    testRenderer = null;
-  });
 
-  it('renders', () => {
+  it('renders', async () => {
+    testRenderer.focus();
+    await completeAnimation(tooltip, ['alpha', 'scale']);
     const tree = testRenderer.toJSON(2);
     expect(tree).toMatchSnapshot();
   });
 
   describe('visibility', () => {
-    afterEach(() => {
-      jest.clearAllTimers();
-      jest.useRealTimers();
-    });
+    it('should be visible when focused', async () => {
+      expect(tooltip.alpha).toEqual(0);
+      expect(tooltip.scale).toBe(0.5);
 
-    it('should be visible when focused', () => {
       tooltip._focus();
-      testRenderer.update();
+      await completeAnimation(tooltip, ['alpha', 'scale']);
+
       expect(tooltip.alpha).toEqual(1);
+      expect(tooltip.scale).toEqual(1);
     });
 
-    it('should be hidden when unfocused', () => {
+    it('should be hidden when unfocused', async () => {
+      [tooltip, testRenderer] = createTooltip(
+        { title: 'Tooltip' },
+        { focused: true }
+      );
+      await completeAnimation(tooltip, ['alpha', 'scale']);
+
+      expect(tooltip.alpha).toEqual(1);
+      expect(tooltip.scale).toEqual(1);
+
       tooltip._unfocus();
-      testRenderer.update();
+      await completeAnimation(tooltip, ['alpha', 'scale']);
+
       expect(tooltip.alpha).toEqual(0);
+      expect(tooltip.scale).toBe(0.5);
     });
 
-    it('should show after delay if delayVisible is set', () => {
-      jest.useFakeTimers();
-      expect(tooltip.alpha).toEqual(0);
+    it('should show after delay if delayVisible is set', async () => {
+      const delayVisible = 5000;
+      [tooltip, testRenderer] = createTooltip({ delayVisible });
 
-      // set delayVisible
-      tooltip.delayVisible = 5000;
+      expect(tooltip.alpha).toEqual(0);
+      expect(tooltip.scale).toBe(0.5);
+      expect(tooltip._showTimer).toBeUndefined();
+      expect(setTimeout).not.toHaveBeenCalledWith(
+        expect.any(Function),
+        delayVisible
+      );
+
+      tooltip._focus();
 
       // should remain hidden
-      tooltip._focus();
-      testRenderer.update();
-      expect(setTimeout).toHaveBeenCalledTimes(1);
       expect(tooltip.alpha).toEqual(0);
+      expect(tooltip.scale).toBe(0.5);
+      expect(tooltip._showTimer).not.toBeUndefined();
+      expect(setTimeout).toHaveBeenCalledWith(
+        expect.any(Function),
+        delayVisible
+      );
+
+      jest.advanceTimersByTime(delayVisible);
+      await completeAnimation(tooltip, ['alpha', 'scale']);
 
       // should show after timer runs
-      jest.runAllTimers();
-      testRenderer.update();
       expect(tooltip.alpha).toEqual(1);
+      expect(tooltip.scale).toEqual(1);
     });
 
-    it('should hide after timer ends if timeVisible is set', () => {
-      jest.useFakeTimers();
-
-      // timer not run unless focused
-      expect(setTimeout).toHaveBeenCalledTimes(0);
-
-      // show tooltip on focus
-      tooltip._focus();
-      testRenderer.update();
-      expect(tooltip.alpha).toEqual(1);
-
-      // check that no timer has been called and tooltip remains visible
-      jest.runAllTimers();
-      expect(setTimeout).toHaveBeenCalledTimes(0);
-      testRenderer.update();
-      expect(tooltip.alpha).toEqual(1);
-
-      // set timeVisible
-      tooltip.timeVisible = 5000;
-
-      // timer not run unless focused
-      expect(setTimeout).toHaveBeenCalledTimes(0);
+    it('should hide after timer ends if timeVisible is set', async () => {
+      const timeVisible = 5000;
+      expect(tooltip._hideTimer).toBeUndefined();
 
       tooltip._focus();
-      testRenderer.update();
+      await completeAnimation(tooltip, ['alpha', 'scale']);
+
+      expect(setTimeout).not.toHaveBeenCalled();
+      expect(tooltip._hideTimer).toBeUndefined();
       expect(tooltip.alpha).toEqual(1);
+      expect(tooltip.scale).toEqual(1);
+
+      tooltip.timeVisible = timeVisible;
+
+      expect(tooltip._hideTimer).toBeUndefined();
+
+      tooltip._focus();
+      await completeAnimation(tooltip, ['alpha', 'scale']);
+
+      expect(setTimeout).toHaveBeenCalledWith(
+        expect.any(Function),
+        timeVisible
+      );
+      expect(tooltip._hideTimer).not.toBeUndefined();
+      expect(tooltip.alpha).toEqual(1);
+      expect(tooltip.scale).toEqual(1);
 
       // timer started on focus and hide after timer is called
-      jest.runAllTimers();
-      expect(setTimeout).toHaveBeenCalledTimes(1);
-      testRenderer.update();
+      jest.advanceTimersByTime(timeVisible);
+      await completeAnimation(tooltip, ['alpha', 'scale']);
+
       expect(tooltip.alpha).toEqual(0);
-    });
-  });
-
-  describe('position', () => {
-    // eslint-disable-next-line no-unused-vars
-    let button, buttonRenderer;
-    beforeEach(() => {
-      tooltip._focus();
-      // eslint-disable-next-line no-unused-vars
-      [button, buttonRenderer] = createButton({ title: 'Button' });
-      buttonRenderer.update();
-    });
-    it('should default to 40px above parent', done => {
-      tooltip._Text.on('txLoaded', () => {
-        const buffer = 2;
-        expect(tooltip.y).toEqual(-tooltip._Background.finalH - 40 + buffer);
-
-        done();
-      });
-    });
-
-    it('should change when "bottomMargin" is set', done => {
-      const [closer_tooltip] = createTooltip({
-        title: 'Tooltip',
-        bottomMargin: 20
-      });
-      closer_tooltip._focus();
-
-      tooltip._Text.on('txLoaded', () => {
-        closer_tooltip._Text.on('txLoaded', () => {
-          expect(closer_tooltip.y).toEqual(tooltip.y + 20);
-          done();
-        });
-      });
+      expect(tooltip.scale).toBe(0.5);
     });
   });
 
   describe('dimensions', () => {
-    // NOTE: the font doesn't text size and calculated tooltip size is not accurate
-
-    beforeEach(() => {
+    beforeEach(async () => {
       tooltip._focus();
+      await completeAnimation(tooltip, ['alpha', 'scale']);
     });
 
-    it('should be slightly larger than text dimensions', done => {
-      testRenderer.update();
-
-      setTimeout(() => {
-        try {
-          const textWidth = tooltip._Text.finalW;
-          const textHeight = tooltip._Text.finalH;
-
-          const diffW = Math.abs(tooltip._Text.finalW - textWidth);
-          expect(diffW).toBeLessThan(5);
-          expect(tooltip._Background.finalW).toBeGreaterThan(textWidth);
-
-          const diffH = Math.abs(tooltip._Background.finalH - textHeight);
-          expect(diffH).toBeLessThan(5);
-
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 1000);
+    it('should give the tooltip a margin bottom', () => {
+      expect(tooltip.y).toBeLessThan(0);
     });
 
-    it('should be narrower when the message text changes to a shorter message', done => {
+    it('should grow in width for longer text', () => {
+      const initialW = tooltip._Background.w;
+      tooltip.title = 'Longer Tooltip';
+      jest.runAllTimers();
       testRenderer.update();
-
-      const controlW = tooltip.finalW;
-      const controlBackgroundW = tooltip._Background.finalW;
-
-      tooltip.title = 'short';
-
-      tooltip._Text.loadTexture();
-      tooltip._Background.loadTexture();
-      testRenderer.update();
-
-      setTimeout(() => {
-        try {
-          expect(tooltip.finalW).toBeLessThan(controlW);
-          expect(tooltip._Background.finalW).toBeLessThan(controlBackgroundW);
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 1000);
-    });
-
-    it('should have a longer hint message when the text changes', done => {
-      testRenderer.update();
-
-      const controlW = tooltip.finalW;
-      const controlBackgroundW = tooltip._Background.finalW;
-
-      tooltip.title = 'a longer title';
-
-      tooltip._Text.loadTexture();
-      tooltip._Background.loadTexture();
-      testRenderer.update();
-
-      setTimeout(() => {
-        try {
-          expect(tooltip.finalW).toBeGreaterThan(controlW);
-          expect(tooltip._Background.finalW).toBeGreaterThan(
-            controlBackgroundW
-          );
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 1000);
-    });
-
-    it('should change height and wrap text when hint message is long', done => {
-      testRenderer.update();
-
-      const controlH = tooltip._Text.finalH;
-      const controlBackgroundH = tooltip._Background.finalH;
-
-      tooltip.title =
-        'This text should render in two lines...This text should render in two lines...This text should render in two lines...This text should render in two lines...This text should render in two lines...This text should render in two lines...This text should render in two lines...This text should render in two lines...This text should render in two lines...This text should render in two lines...This text should render in two lines...';
-
-      tooltip._Text.loadTexture();
-      tooltip._Background.loadTexture();
-      testRenderer.update();
-
-      setTimeout(() => {
-        try {
-          const textHeight = tooltip._Text.finalH;
-          const backgroundHeight = tooltip._Background.finalH;
-
-          const diffH = Math.abs(tooltip._Background.finalH - textHeight);
-
-          expect(diffH).toBeLessThan(5);
-
-          expect(textHeight).toBeGreaterThan(controlH);
-          expect(backgroundHeight).toBeGreaterThan(controlBackgroundH);
-
-          done();
-        } catch (error) {
-          done(error);
-        }
-      }, 1000);
+      expect(tooltip._Background.w).toBeGreaterThan(initialW);
     });
   });
 });
