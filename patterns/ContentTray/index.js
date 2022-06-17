e68e899489b9cd58e9de190afa29ba49e1f3745b
Crossman, Alexandra (Contractor) <Alexandra_Crossman@comcast.com>
2022-06-17 11:27:42 -0400

    feat(ContentTray): ContentTray pattern soft focus treatment (#988)
    * feat(OTTX-23915): basic file structure for new ContentTray pattern
    
    * feat(OTTX-23915): work in progress on ContentTray
    
    * feat(OTTX-23915): work in progress on ContentTray
    
    * feat(OTTX-23915): work in progress on ContentTray
    
    * feat(OTTX-23915): work in progress on ContentTray
    
    * feat(OTTX-23915): work in progress on ContentTray with much help from Josh!
    
    * feat(OTTX-23915): updating tabs to use (mostly) tiles
    
    * feat(OTTX-23915): update ContentTray story to use CardLaunchpad
    
    * feat(OTTX-23915): restore updating tabs to use tiles
    
    * feat(OTTX-23915): updating tray content to use launchpad cards
    
    * feat(OTTX-23915): an attempt was made at the view all button
    
    * feat(OTTX-23815): fixing up the action button; to-do: ActionButtonXsmall icon size
    
    * feat(OTTX-23815): setup ContentTray tests
    
    * feat(OTTX-23815): update ContentTray tests and cleanup code
    
    * feat(OTTX-23815): update ContentTray tests
    
    * feat(OTTX-23815): update ContentTray test
    
    * feat(OTTX-23915): updating .mdx, adjusting action button
    
    * feat(OTTX-23815): update ContentTray tests
    
    * feat(OTTX-25173): rough pass at contenttray soft focus
    
    * feat(OTTX-25173): cleaning comments
    
    * fix(FocusManager): when the Items container is reset, also reset its x and y positioning
    
    * feat(OTTX-25173): updating ContentTray spacing, adding transition
    
    * fix(OTTX-25173): set initial selected Results index for initial component load
    
    * fix(OTTX-25173): fix unit tests for animations
    
    * fix(OTTX-25173): fix ContentTray unit test
    
    * fix(OTTX-25173): update tests to account for column changed
    
    * feat(OTTX-25173): cleaning up styles, _columnChanged, comments
    
    * fix(OTTX-25173): fix grammar in comment
    
    * feat(OTTX-25173): further refinement
    
    Co-authored-by: Alex Crossman <across106@cable.comcast.com>
    Co-authored-by: Michael Angeli <michael_angeli2@comcast.com>
    Co-authored-by: eraute200 <emily_rautenberg@comcast.com>

--- patterns/ContentTray/index.js
+++ patterns/ContentTray/index.js
@@ -1,13 +1,16 @@
 import Base from '../../elements/Base';
 import { Row, Column } from '../../layout';
+import styles from './ContentTray.styles';
+import withStyles from '../../mixins/withStyles';
 
-export default class ContentTray extends Base {
+export default class ContentTray extends withStyles(Base, styles) {
   static _template() {
     return {
       Container: {
         type: Column,
         signals: {
-          tabChanged: '_tabChanged'
+          tabChanged: '_tabChanged',
+          selectedChange: '_columnChanged'
         },
         Items: {
           Tabs: {
@@ -53,6 +56,27 @@ export default class ContentTray extends Base {
     this._results = []; // This will be filled when items is set
   }
 
+  _columnChanged() {
+    // filtering for tabs that are not the current focused tab
+    const focusedIndex = this._Tabs.items[this._selectedResultsIndex];
+    const unfocused = this._Tabs.items.filter(x => x !== focusedIndex);
+
+    // results row is in focus, add soft focus treatment and fade inactive tabs
+    if (this._Container.selectedIndex === 1) {
+      focusedIndex.patch({ SoftFocus: this.styles.softFocus });
+      unfocused.map(tab => {
+        tab.patch(this.styles.fadedTabs);
+      });
+      return;
+    }
+
+    // tabs row focused, hide soft focus and reset tabs alpha
+    focusedIndex.patch({
+      SoftFocus: this.styles.hideSoftFocus
+    });
+    unfocused.map(tab => tab.patch(this.styles.visibleTabs));
+  }
+
   _setItems(v) {
     if (!Array.isArray(v)) {
       console.info(
@@ -67,58 +91,74 @@ export default class ContentTray extends Base {
       delete tab.items; // delete child items as they are now stored in _results; this will be referenced by _updateResults by the selected top item's index
       return tab; // done going through top level items
     });
-
+    this._selectedResultsIndex = 0; // Set the initial selected index
     return tabItems;
   }
 
   _update() {
-    this._Container.patch({ w: this.w, ...this._layoutPatch });
+    this._Container.patch({
+      w: this.w,
+      ...this._layoutPatch,
+      itemSpacing: this.styles.verticalSpacing
+    });
     this._updateTabs();
     this._updateResults();
   }
 
   _updateTabs() {
-    this._Tabs.patch({ w: this.w, ...this._layoutPatch });
+    this._Tabs.patch({
+      w: this.w,
+      ...this._layoutPatch,
+      itemSpacing: this.styles.tabSpacing
+    });
     this._Tabs.items = this.items;
     this._Tabs.h = this._Tabs.Items.h; // Make sure the height renders correctly
   }
 
-  _updateResults(index = 0) {
-    this._Results.patch({ w: this.w, ...this._layoutPatch });
+  _updateResults() {
+    this._Results.smooth = { alpha: 0.001 };
+    this._Results._getTransition('alpha').once('finish', () => {
+      this._Results.patch({
+        w: this.w,
+        ...this._layoutPatch,
+        itemSpacing: this.styles.resultSpacing
+      });
 
-    if ('function' === typeof this._results[index]) {
-      // if the results are being returned from a promise
-      const resultsFunc = this._results[index]; // store promise
-      const results = resultsFunc(); // execute promise
-      if (results.then) {
-        this._Results.items = []; // Clear current results
-        // Looks like a promise
-        results.then(items => {
-          // Make sure still on the current tab
-          if (resultsFunc === this._results[this._Tabs._selectedIndex]) {
-            this._Results.items = items; // set results children to what promise returned
-            this._updateResultsLayout();
-          }
-        });
+      if ('function' === typeof this._results[this._selectedResultsIndex]) {
+        // if the results are being returned from a function
+        const resultsFunc = this._results[this._selectedResultsIndex]; // store function
+        const results = resultsFunc(); // execute function
+        if (results.then) {
+          this._Results.items = []; // Clear current results
+          results.then(items => {
+            // Make sure still on the current tab
+            if (resultsFunc === this._results[this._Tabs._selectedIndex]) {
+              this._Results.items = items; // get results from Promise function
+              this._updateResultsLayout();
+            }
+          });
+        } else {
+          // get results from Synchronous function
+          this._Results.items = results;
+          this._updateResultsLayout();
+        }
       } else {
-        // Is synchronous
-        this._Results.items = results;
+        this._Results.items = this._results[this._selectedResultsIndex]; // set results children to what is stored in _results
         this._updateResultsLayout();
       }
-    } else {
-      this._Results.items = this._results[index]; // set results children to what is stored in _results
-      this._updateResultsLayout();
-    }
+    });
   }
 
   _updateResultsLayout() {
     this._Results.skipFocus = !this._Results.items.length; // Make sure to skip focus if no results for first time
     // todo: make sure we switch the following to use autoResizeHeight when merging into 'next' branch
     this._Results.h = this._Results.Items.h; // Make sure the height renders correctly
+    this._Results.smooth = { alpha: 1 };
   }
 
   _tabChanged() {
-    this._updateResults(this._Tabs._selectedIndex);
+    this._selectedResultsIndex = this._Tabs._selectedIndex;
+    this._updateResults();
   }
 
   _getFocused() {
