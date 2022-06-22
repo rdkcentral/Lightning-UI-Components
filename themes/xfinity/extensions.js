79efb975261c89f6641f4ea3bfbf8239de8608f5
Howenstine, Joshua <Joshua_Howenstine@comcast.com>
2022-06-22 14:55:11 -0700

    fix(Theme): update extensions to support prototypal inheritance (#993)
    * fix(Theme): update extensions to support prototypal inheritance
    
    * fix: linting corrections
    
    * fix(LUI-443): move _prototypeChain logic to getter

--- themes/xfinity/extensions.js
+++ themes/xfinity/extensions.js
@@ -2,19 +2,7 @@ import focusRingExtension from '../../extensions/FocusRing.extension';
 
 export default [
   {
-    targetComponent: [
-      'Knob',
-      'Surface',
-      // TODO: All extensions from a parent class to apply to extended classes and remove the below
-      // (ie Tiles and Cards should automatically get the focus ring because they extends Surface)
-      'Tile',
-      'NewCard',
-      'CardTitle',
-      'CardSection',
-      'AboutCard',
-      'AboutStackedCard',
-      'CardPersonality'
-    ],
+    targetComponent: ['Surface', 'Knob'],
     extension: focusRingExtension
   }
 ];
