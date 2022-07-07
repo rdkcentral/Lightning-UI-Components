32ea54a254dadc034254695d0f9a6f7f1277e115
Harshita Bhatt <harshitabhatt58@gmail.com>
2022-07-07 12:50:34 -0400

    fix(CardLaunchpad): Launchpad Card should emit (#1002)
    * fix(CardLaunchpad): Launchpad Card should emit
    
    * fix(CardLaunchpad): resolved Emily's comments
    
    * fix(CardLaunchpad): removed update method and fixed it if you go to apps tray and back down
    
    * fix(CardLaunchpad): reverted rows story
    
    * fix(CardLaunchpad): updated _firstEnable method
    
    * fix(CardLaunchpad): updated setCardDimension with Emily's code

--- patterns/Card/CardLaunchpad.js
+++ patterns/Card/CardLaunchpad.js
@@ -1,9 +1,21 @@
 import { CardHorizontal } from '.';
 
 class CardLaunchpad extends CardHorizontal {
-  _update() {
-    super._update();
-    this.collapseData = !this.hasFocus();
+  _construct() {
+    super._construct();
+    this.collapseData = true;
+  }
+
+  _focus() {
+    this.collapseData = false;
+    this._update();
+    this.fireAncestors('$itemChanged');
+  }
+
+  _unfocus() {
+    this.collapseData = true;
+    this._update();
+    this.fireAncestors('$itemChanged');
   }
 }
 
