90861dbdd547f635cc424fbcf6ab49028435e80b
Harshita Bhatt <harshitabhatt58@gmail.com>
2022-07-08 13:59:20 -0400

    fix(TranslationScript): fixed infinite loop, broke it into functions, and included patterns (#997)
    * fix(TranslationScript): fixed infinite loop, broke it up, and includes patterns
    
    * fix(TranslationScript): updated package-json
    
    * fix(TranslationScript): created more functions
    
    * fix(TranslationScript): updated package.json files
    
    * fix(TranslationScript): added a space
    
    * fix(TranslationScript): updated for Josh
    
    * fix(TranslationScript): still need to update some of Josh's comments
    
    * fix(TranslationScript): added in roles table
    
    * fix(TranslationScript): resolved Emily's comments
    
    * Trigger Concourse

---
+++ bin/scriptingFiles/colors.md
@@ -0,0 +1,32 @@
+colors Value | Components | Role 
+--------|--------|--------
+material | [Artwork](../?path=/docs/elements-artwork--basic)<br>[Label](../?path=/docs/elements-label--basic)|Default page background
+coreInverseSecondary | [Badge](../?path=/docs/elements-badge--text)<br>[Metadata](../?path=/docs/elements-metadata--basic)<br>[NewCard](../?path=/docs/patterns-newcard--base)<br>[Provider](../?path=/docs/elements-provider--basic)<br>[Surface](../?path=/docs/elements-surface--basic)|Secondary text <br> Secondary icons <br> Unfocused states
+coreNeutral | [AboutCard](../?path=/docs/patterns-aboutcard--about-cards)<br>[AboutStackedCard](../?path=/docs/patterns-aboutstackedcard--about-stacked-cards)<br>[Badge](../?path=/docs/elements-badge--text)<br>[CardPersonality](../?path=/docs/patterns-cardpersonality--base)<br>[Checkbox](../?path=/docs/elements-checkbox--basic)<br>[FocusRing](../?path=/docs/elements-focusring--basic)<br>[Icon](../?path=/docs/elements-icon--basic)<br>[Label](../?path=/docs/elements-label--basic)<br>[Metadata](../?path=/docs/elements-metadata--basic)<br>[NewCard](../?path=/docs/patterns-newcard--base)<br>[ProgressBar](../?path=/docs/elements-progressbar--basic)<br>[Radio](../?path=/docs/elements-radio--basic)<br>[Slider](../?path=/docs/elements-slider--basic)<br>[Toggle](../?path=/docs/elements-toggle--basic)<br>[Tooltip](../?path=/docs/elements-tooltip--basic)|Primary text <br> Primary icons
+coreNeutralSecondary | [AboutCard](../?path=/docs/patterns-aboutcard--about-cards)<br>[Badge](../?path=/docs/elements-badge--text)<br>[CardPersonality](../?path=/docs/patterns-cardpersonality--base)<br>[CardTitle](../?path=/docs/patterns-cardtitle--title-and-description)<br>[Metadata](../?path=/docs/elements-metadata--basic)<br>[Provider](../?path=/docs/elements-provider--basic)|Secondary text <br> Secondary icons <br> Unfocused states
+coreInverse | [Badge](../?path=/docs/elements-badge--text)<br>[Checkbox](../?path=/docs/elements-checkbox--basic)<br>[FocusRing](../?path=/docs/elements-focusring--basic)<br>[Icon](../?path=/docs/elements-icon--basic)<br>[Label](../?path=/docs/elements-label--basic)<br>[Metadata](../?path=/docs/elements-metadata--basic)<br>[ProgressBar](../?path=/docs/elements-progressbar--basic)<br>[Radio](../?path=/docs/elements-radio--basic)<br>[Slider](../?path=/docs/elements-slider--basic)<br>[Toggle](../?path=/docs/elements-toggle--basic)<br>[Tooltip](../?path=/docs/elements-tooltip--basic)|Primary text <br> Primary icons
+coreBrand | [Badge](../?path=/docs/elements-badge--text)<br>[Checkbox](../?path=/docs/elements-checkbox--basic)<br>[FocusRing](../?path=/docs/elements-focusring--basic)<br>[Icon](../?path=/docs/elements-icon--basic)<br>[Label](../?path=/docs/elements-label--basic)<br>[ProgressBar](../?path=/docs/elements-progressbar--basic)<br>[Radio](../?path=/docs/elements-radio--basic)<br>[Toggle](../?path=/docs/elements-toggle--basic)<br>[Tooltip](../?path=/docs/elements-tooltip--basic)|Primary brand accent
+coreInverseDisabled | [Checkbox](../?path=/docs/elements-checkbox--basic)<br>[NewCard](../?path=/docs/patterns-newcard--base)<br>[Radio](../?path=/docs/elements-radio--basic)<br>[Surface](../?path=/docs/elements-surface--basic)<br>[Toggle](../?path=/docs/elements-toggle--basic)|Disabled text <br> Disabled icons <br> Disabled states
+coreNeutralDisabled | [Checkbox](../?path=/docs/elements-checkbox--basic)<br>[NewCard](../?path=/docs/patterns-newcard--base)<br>[Radio](../?path=/docs/elements-radio--basic)<br>[Toggle](../?path=/docs/elements-toggle--basic)|Disabled text <br> Disabled icons <br> Disabled states
+coreNeutralTertiary | [FocusRing](../?path=/docs/elements-focusring--basic)<br>[ProgressBar](../?path=/docs/elements-progressbar--basic)|Tertiary text <br> Tertiary icons
+coreInverseTertiary | [FocusRing](../?path=/docs/elements-focusring--basic)<br>[ProgressBar](../?path=/docs/elements-progressbar--basic)|Tertiary text <br> Tertiary icons
+coreBrandTertiary | [FocusRing](../?path=/docs/elements-focusring--basic)<br>[ProgressBar](../?path=/docs/elements-progressbar--basic)|More info coming
+interactiveNeutralFocus | [Knob](../?path=/docs/elements-knob--basic)<br>[Slider](../?path=/docs/elements-slider--basic)<br>[Surface](../?path=/docs/elements-surface--basic)|More info coming
+interactiveInverseFocus | [Knob](../?path=/docs/elements-knob--basic)<br>[Slider](../?path=/docs/elements-slider--basic)<br>[Surface](../?path=/docs/elements-surface--basic)|More info coming
+interactiveBrandFocus | [Surface](../?path=/docs/elements-surface--basic)|More info coming
+interactiveNeutralFocusSoft | [NewCard](../?path=/docs/patterns-newcard--base)|More info coming
+interactiveInverseFocusSoft | [NewCard](../?path=/docs/patterns-newcard--base)|More info coming
+interactiveBrandFocusSoft | [NewCard](../?path=/docs/patterns-newcard--base)|More info coming
+materialBrand | No components are using this value |Branded page background
+coreBrandSecondary | No components are using this value |Unfocused states
+coreBrandDisabled | No components are using this value |Disabled states
+coreOverlay | No components are using this value |Overlay dim
+black | No components are using this value |More info coming
+white | No components are using this value |More info coming
+gray | No components are using this value |More info coming
+red | No components are using this value |More info coming
+orange | No components are using this value |More info coming
+yellow | No components are using this value |More info coming
+green | No components are using this value |More info coming
+blue | No components are using this value |More info coming
+purple | No components are using this value |More info coming
--- bin/translationScript.js
+++ bin/translationScript.js
@@ -1,125 +1,219 @@
 const fs = require('fs');
-const readline = require('readline');
+import { getStorybookLinks } from './getStorybookLinks.js';
+import colorsJsonFile from './colorsRole.json';
+//You could change the path and get the values for different themes
 const radius = require('../themes/base/radius.js');
 const typography = require('../themes/base/typography.js');
 const colors = require('../themes/base/colors.js');
-createTable('radius', 'bin/scriptingFiles/radiusTable.md', radius);
-createTable('typography', 'bin/scriptingFiles/typographyTable.md', typography);
-createTable('colors', 'bin/scriptingFiles/colorsTable.md', colors);
+const LineReaderSync = require('line-reader-sync');
 
-function createTable(themeProperty, mdFile, themeFiles) {
+createDocumentation(); //This call starts the scripts to create the documentation
+/**
+ * Creates documentation for the colors, typography, and radius that our components (both elements and patterns) use.
+ * hypothetically if you replace the parameters with other values like (stroke, 'tmp?scriptingFiles/strokeTable.md, stroke) you would get all the stroke values and which components are using what stroke value. 
+ */
+export function createDocumentation() {
+  getStyleFiles('colors', 'bin/scriptingFiles/colors.md', colors);
+  getStyleFiles('typography', 'bin/scriptingFiles/typography.md', typography);
+  getStyleFiles('radius', 'bin/scriptingFiles/radius.md', radius);
+}
+
+/**
+ * Combines all the necessary files and theme values
+ * @param {string} themeProperty - what theme property we are looking for ie). colors, radius, typography
+ * @param {string} mdFile - the markdown file the contents will be written to 
+ * @param {string} themeFiles - files that hold the possible theming values ie). theme.colors.fillBrand, or theme.radius.xsmall
+ */
+export function getStyleFiles(themeProperty, mdFile, themeFiles) {
+  const fileOutput = fs.readdirSync('./elements');
+  const fileOutputPatterns = fs.readdirSync('./patterns');
+  const fileOutputLayout = fs.readdirSync('./layout');
+  const elementStyleFiles = getComponentFiles(fileOutput, 'elements');
+  const patternStyleFiles = getComponentFiles(fileOutputPatterns, 'patterns');
+  const layoutStyleFiles = getComponentFiles(fileOutputLayout, 'layout');
+  const styleFileArray = [...elementStyleFiles, ...patternStyleFiles, ...layoutStyleFiles];//combines the elements and patterns array
+  const themeValueArray = getThemeProperties(themeFiles, themeProperty);
+  findThemedComponents(styleFileArray, themeValueArray, themeProperty, mdFile);
+}
+
+/**
+ * Grabs all the style files for both the patterns and elements
+ * @param {string} fileList - list of all the components and their respective files
+ * @param {string} componentType - type of component (element or pattern)
+ * @returns {array} array with all the components style files
+ */
+export function getComponentFiles(fileList, componentType) {
+  return fileList.reduce((acc, file) => {
+    const folder = `./${componentType}/` + file;
+    if (!fs.statSync(folder).isDirectory()) return acc;
+    const styleFiles = (fs.readdirSync(folder) || [])
+      .filter(fileName => fileName.endsWith('.styles.js'))
+      .map(fileName => `./${componentType}/${file}/${fileName}`);
+    return [...acc, ...styleFiles];
+  }, []);
+}
+
+/**
+ * Grabs all the values for theme properties. For example radius: xmall, color: coreBrand, etc.
+ * @param {string} themeFiles - files that hold the possible theming values ie). theme.colors.fillBrand, or theme.radius.xsmall
+ * @param {string} componentType - type of component (element or pattern)
+ * @returns {array} array with all the components style files
+ */
+export function getThemeProperties(themeFiles, themeProperty) {
   const themeKeys = themeFiles;
-  const dict = {};
   const themeValueArray = [];
   const themeValueModifiedArray = Object.keys(themeKeys.default);
   const str = `theme.${themeProperty}.`;
+  //gets all of the theme values in format in how they are in the style files
   for (const key in themeValueModifiedArray) {
     const temporaryKey = str.concat(themeValueModifiedArray[key]);
     themeValueArray.push(temporaryKey);
   }
-  const styleFileArray = [];
-  const variantsArray = ['neutral:', 'inverse:', 'brand:'];
-  let fileOutput = fs.readdirSync('./elements');
-  fileOutput = fileOutput.filter(file => {
-    return file !== 'index.d.ts' && file !== 'index.js'
-  });
-  const filteredFileOutput = fileOutput.filter(file => {
-    return !file.endsWith('.js');
-  });
-  filteredFileOutput.forEach(file => {
-    fs.readdirSync('./elements/' + file).forEach(file1 => {
-      try {
-        fs.statSync('./elements/' + file + '/' + file1);
-        if ((`./elements/${ file }/${ file1 }`).endsWith('.styles.js')) {
-          const styleFileName = `./elements/${ file }/${ file1 }`;
-          styleFileArray.push(styleFileName);
-        }
-      } catch (err) {
-        if (err.code === 'ENOTDIR') {
-          console.log(
-            'file or directory does not exist',
-            `./elements/${file}/${file1}`
-          );
-        }
-      }
-    });
-  });
-  Promise.all(
-    styleFileArray.map(file => {
-      return new Promise(resolve => {
-        const file1 = readline.createInterface({
-          input: fs.createReadStream(file),
-          output: process.stdout,
-          terminal: false
-        });
-        let variantFound = undefined;
-        file1.on('line', line => {
-          const valueFound = themeValueArray.some(val => line.includes(val));
-          const temp = variantsArray.find(val => line.includes(val));
-          if (temp) {
-            variantFound = temp.substring(0, temp.length - 1);
-          }
-          if (valueFound) {
-            const updateFileName = file.split('/');
-            line = line.trim();
-            line = line.split(/[-=/_+*]/)
-            for (let i = 0; i < line.length; i++) {
-              let foundIndex = 0;
-              const newLine = line[i].split('.');
-              for (let j = newLine.length - 1; j >= 0; j--){
-                newLine[j] = newLine[j].includes(',')
-                  ? newLine[j].substring(0, newLine[j].length - 1)
-                  : newLine[j];
-                 newLine[j] = newLine[j].includes('}')
-                  ? newLine[j].substring(0, newLine[j].length - 1)
-                  : newLine[j];
-                newLine[j] = newLine[j].includes(' ')
-                  ? newLine[j].substring(0, newLine[j].length - 1)
-                  : newLine[j];
-                const foundCorrectIndex = themeValueArray.some(val => val.includes(newLine[j]));
-                if (foundCorrectIndex) {
-                  foundIndex = j;
-                  break;
-                }
-              }
-              const updatedLine = newLine[foundIndex];
-              if (isNaN(updatedLine)) {
-                const newFileName = updateFileName[updateFileName.length - 1];
-                const separatedFileName = newFileName.split('.');
-                const finalFileName = separatedFileName[0];
-                if (dict[updatedLine]) {
-                  if (!variantFound) {
-                    dict[updatedLine] = `<li> ${dict[updatedLine]} </li>, <li>${finalFileName}</li>`;
-                    dict[updatedLine] = dict[updatedLine].trim();
-                    const removeDuplicates = dict[updatedLine].split(', ');
-                    const duplicates = [...new Set(removeDuplicates)];
-                    dict[updatedLine] = duplicates.join('</li>, ');
+  return themeValueArray;
+}
 
-                  }
-                  else {
-                      dict[updatedLine] =
-                    `<li> ${dict[updatedLine]},</li> ${finalFileName} (${variantFound})`;
-                  }
-                }
-                else {
-                  dict[updatedLine] = variantFound ? `<li> ${finalFileName} (${variantFound}) </li>` : dict[updatedLine] = `<li> ${finalFileName} </li>`;
-                }
+/**
+ * Finds all the elements and patterns and what style value they are using
+ * @param {array} styleFileArray - array that holds all the component's style files
+ * @param {array} themeValueArray - array that holds all the possible themeing values for each theme property
+ * @param {string} themeProperty - what theme property we are looking for ie). colors, radius, typography
+ * @param {string} mdFile - the markdown file the contents will be written to
+ */
+export function findThemedComponents(styleFileArray, themeValueArray, themeProperty, mdFile) {
+  const dict = {};
+
+  for (let i = 0; i < styleFileArray.length; i++) {
+    const updateFileName = styleFileArray[i].split('/');
+    const name = updateFileName[updateFileName.length - 1].split('.')[0];
+    let componentName = updateFileName;
+    componentName = componentName.splice(1, 2);
+    const linkString = componentName.join('/');
+    const docUrl = getStorybookLinks(componentName[1], linkString, name);
+    const lrs = new LineReaderSync(styleFileArray[i]);
+    while (true) { //start searching for the storybook url line
+      let line = lrs.readline();
+      if (line === null) {
+        break;
+      }
+      else if (themeValueArray.some(val => line.includes(val))){
+          //we split the line up by above characters to account for cases like theme.radius.medium + theme.radius.small
+          line = line.split(/[-=/_+*]/);
+          for (let i = 0; i < line.length; i++) {
+            let foundIndex = 0;
+            const newLine = line[i].split('.');
+            foundIndex = cleanLine(newLine, themeValueArray);
+            const updatedLine = newLine[foundIndex];
+            if (isNaN(updatedLine)) {
+              const newFileName = updateFileName[updateFileName.length - 1];
+              const finalFileName = getFinalFileName(newFileName, componentName);
+              //if there are already components using the theme property, the new component using that theme property needs to be appended differently
+              if (dict[updatedLine]) {
+                dict[updatedLine] =
+                  `${dict[updatedLine]}, [${finalFileName}](${docUrl})`;
+                dict[updatedLine] = cleanDictionaryValues(dict, updatedLine);
+              }
+              else {
+                dict[updatedLine] = `[${finalFileName}](${docUrl})`;
               }
             }
           }
-          resolve();
-        });
-      });
-    })
-  ).then(() => {
-    const content = `${themeProperty} Value | Components \n`;
-    fs.writeFileSync(mdFile, content);
-    const header = '--------|-------- \n';
+      }
+    }
+  }
+  writeMarkdownFiles(themeProperty, mdFile, dict, themeValueArray);
+}
+
+/**
+ * Finds all the elements and patterns and what style value they are using
+ * @param {string} newLine - string that needs to be cleaned and checked for possible theme value
+ * @param {array} themeValueArray - array that holds all the possible themeing values for each theme property
+ * @returns {number} returns index where theme value is found
+ */
+export function cleanLine(newLine, themeValueArray) {
+  let foundIndex; 
+  for (let j = newLine.length - 1; j >= 0; j--) {
+      newLine[j] = newLine[j].trim();
+      newLine[j] = newLine[j].includes(',')
+        ? newLine[j].substring(0, newLine[j].length - 1)
+        : newLine[j];
+      newLine[j] = newLine[j].includes('}')
+        ? newLine[j].substring(0, newLine[j].length - 1)
+        : newLine[j];
+      newLine[j] = newLine[j].trim();
+      //is able to get the correct themeValue found in the line
+      const foundCorrectIndex = themeValueArray.some(val => val.includes(newLine[j]));
+      if (foundCorrectIndex) {
+        foundIndex = j;
+        return foundIndex;
+      }
+  }
+}
+
+/**
+ * Finds all the elements and patterns and what style value they are using
+ * @param {string} newFileName - file name that needs to be parsed
+ * @param {string} componentName - name of the component in case file name is index.js
+ * @returns {string} returns correct file name
+ */
+export function getFinalFileName(newFileName, componentName) { 
+  const separatedFileName = newFileName.split('.');
+    let finalFileName = separatedFileName[0];
+    //edge cases because of how some style files are named
+      if (finalFileName === 'index') {
+        finalFileName = componentName[componentName.length-1];
+      }
+  return finalFileName;
+}
+
+/**
+ * Cleans up the values stored in the object that holds the theme values and the components that use them
+ * @param {object} dict - object with theme values and components that use them
+ * @param {string} updatedLine - line that has the theme value that is being used
+ * @returns {string} returns cleaned up line with no duplicate components
+ */
+export function cleanDictionaryValues(dict, updatedLine){
+  dict[updatedLine] = dict[updatedLine].trim();
+  const removeDuplicates = dict[updatedLine].split(', ');
+  //removes duplicates so that there aren't multiple instances of the same component for the same theme property
+  const duplicates = [...new Set(removeDuplicates)];
+  const cleanedUpLine = duplicates.join(', ');
+  return cleanedUpLine;
+}
+
+/**
+ * Cleans up the values stored in the object that holds the theme values and the components that use them
+ * @param {object} dict -object with theme values as the key and components that use them as the value
+ * @param {string} updatedLine - line that has the theme value that is being used
+ * @returns {string} returns cleaned up line with no duplicate components
+ */
+export function writeMarkdownFiles(themeProperty, mdFile, dict, themeValueArray) {
+  const colorFile = mdFile === 'bin/scriptingFiles/colors.md';
+  const content = colorFile? `${themeProperty} Value | Components | Role \n`:`${themeProperty} Value | Components \n`;
+    //ensure folder exists
+    fs.mkdir(mdFile.substring(0, mdFile.lastIndexOf('/')), { recursive: true }, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    fs.writeFileSync(mdFile, content, {encoding:'utf8',flag:'w'});
+    // needed for a markdown file below the header
+    const header = colorFile ?'--------|--------|--------\n' :'--------|-------- \n';
     fs.appendFileSync(mdFile, header);
     for (const key in dict) {
       let modifiedKey = `theme.${themeProperty}.`;
       modifiedKey = modifiedKey.concat(key);
-      const valuesAndComponents = `${key} | ${dict[key]}`;
+      // additional check to make sure only official theme properties are present in the table
+      if (themeValueArray.indexOf(modifiedKey) === -1) {
+        delete dict[key];
+        continue;
+      }
+      //alphabetizes the components using a specific theme property
+      let valuesAndComponents = alphabetizeContent(dict, key);
+      if (colorFile) {
+        const role = getPropertyRole(key);
+        valuesAndComponents = valuesAndComponents.concat(role);
+      }
+      //everything is appended to the file
       fs.appendFileSync(mdFile, valuesAndComponents);
       fs.appendFileSync(mdFile, '\n');
       const duplicates = themeValueArray.some(val =>
@@ -132,9 +226,38 @@ function createTable(themeProperty, mdFile, themeFiles) {
     for (const element of themeValueArray) {
       const temp = element.split('.');
       const temporary = temp[temp.length - 1];
-      const unusedThemeValue = `${temporary} | No components are using this value`;
+       //all theme properties that are currently not being used are added to the bottom
+      const unusedThemeValue = colorFile ? `${temporary} | No components are using this value ${getPropertyRole(temporary)}`:`${temporary} | No components are using this value`;
       fs.appendFileSync(mdFile, unusedThemeValue);
       fs.appendFileSync(mdFile, '\n');
     }
-  });
+}
+
+/**
+ * Alphabetizes the order of the components in each row
+ * @param {object} dict - object with theme values as the key and components that use them as the value
+ * @param {string} key - value in the object
+ * @returns {string} string to be written into the markdown file
+ */
+export function alphabetizeContent(dict, key) {
+  let alphabetizeArray = dict[key].split(',');
+    for (let i = 0; i < alphabetizeArray.length; i++){
+      alphabetizeArray[i] = alphabetizeArray[i].trim();
+    }
+    alphabetizeArray = alphabetizeArray.sort();
+    const alphabetizeString = alphabetizeArray.join(',');
+    //to make the table cleaner all commas are replaced by a newline
+    const noCommaLine = alphabetizeString.replace(/,/g, '<br>');
+    const valuesAndComponentsContent = `${key} | ${[noCommaLine]}`;
+  return valuesAndComponentsContent;
+  
+}
+
+/**
+ * Gets additional info about a theme value based off of Mike's figma files
+ * @param {key} key - value in the object
+ * @returns {string} extra information about the theme property
+ */
+export function getPropertyRole(key) {
+  return `|${colorsJsonFile[key] || 'More info coming'}`;
 }
