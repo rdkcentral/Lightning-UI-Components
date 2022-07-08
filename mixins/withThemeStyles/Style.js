afa36a7704c9f9c5b51c4fc5f968938a332ac593
Josh Howenstine <Joshhowenstine@gmail.com>
2022-07-08 11:57:35 -0700

    fix(Theming): allow custom properties in component style object (#984)
    * fix(LUI-428): allow custom properties in component style object
    
    * fix(LUI-428): change check for custom properties

--- mixins/withThemeStyles/Style.js
+++ mixins/withThemeStyles/Style.js
@@ -3,35 +3,49 @@ import { getValFromObjPath } from '../../utils';
 export default class Style {
   constructor(parent) {
     this._parent = parent;
-
+    this.customProperties = [];
     /**
      * Setup getters and setters on component to override style values
      */
     parent._styleProps.forEach(name => {
-      const key = `_${name}`;
-      Object.defineProperty(this, name, {
-        get() {
-          return this[key] || parent._componentStyles[name];
-        },
-        set(value) {
-          const oldValue = this[key];
-          if (value !== oldValue) {
-            // Check if the value is a string and if it looks like a theme value
-            if ('string' === typeof value && value.includes('theme.')) {
-              const newValue = getValFromObjPath(parent, value);
-              if ('undefined' !== typeof newValue) {
-                this[key] = newValue;
-                this._styleUpdate();
-              }
-            } else {
-              this[key] = value;
+      this._setupProperty(name, false);
+    });
+  }
+
+  _setupProperty(name, custom = true) {
+    if (custom) {
+      if (!this.customProperties.includes(name)) {
+        this.customProperties.push(name);
+      } else {
+        return; // Custom property has already been created
+      }
+    }
+    const key = `_${name}`;
+    Object.defineProperty(this, name, {
+      get() {
+        return (
+          this[key] ||
+          (this._parent._componentStyles && this._parent._componentStyles[name])
+        );
+      },
+      set(value) {
+        const oldValue = this[key];
+        if (value !== oldValue) {
+          // Check if the value is a string and if it looks like a theme value
+          if ('string' === typeof value && value.includes('theme.')) {
+            const newValue = getValFromObjPath(this._parent, value);
+            if ('undefined' !== typeof newValue) {
+              this[key] = newValue;
               this._styleUpdate();
             }
+          } else {
+            this[key] = value;
+            this._styleUpdate();
           }
-        },
-        configurable: true,
-        enumerable: true
-      });
+        }
+      },
+      configurable: true,
+      enumerable: true
     });
   }
 
