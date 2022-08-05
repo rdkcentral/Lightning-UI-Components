9d214cb65df498846093dff37a983c1e4cec3997
Rautenberg, Emily <Emily_Rautenberg@comcast.com>
2022-08-04 22:15:05 -0400

    fix(Download): convert theme colors back to hex and alpha array (#1009)
    * fix(Download): convert theme colors back to hex and alpha array from 10-digit Lightning color code
    
    * feat(Downloader): check all theme props for color values
    
    * feat(Download): update RegEx to only check 10digit color vaue

--- .storybook/utils/index.js
+++ .storybook/utils/index.js
@@ -289,3 +289,32 @@ export function generateSubStory(componentName, BaseStory, SubStory, targetPrope
     ...nestedArgActions(componentName, SubStory.argTypes, targetProperty, ignore)
   };
 }
+
+export function convertColorsObject(colors) {
+  return Object.keys(colors).reduce((acc, curr) => {
+    acc[curr] = convertNumToHexAlphaArray(colors[curr]);
+    return acc;
+  }, {});
+}
+
+export function convertNumToHexAlphaArray(color) {
+  const [r, g, b, a] = getRgbaString(color);
+  return [rgbToHex(r, g, b).toUpperCase(), a * 100];
+}
+
+export function getRgbaString(color) {
+  const r = ((color / 65536) | 0) % 256;
+  const g = ((color / 256) | 0) % 256;
+  const b = color % 256;
+  const a = ((color / 16777216) | 0) / 255;
+  return [r, g, b, a.toFixed(2)];
+};
+
+export function componentToHex(c) {
+  const hex = c.toString(16);
+  return hex.length == 1 ? '0' + hex : hex;
+}
+
+export function rgbToHex(r, g, b) {
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
