a57775864ffcd49c8ba373e627a76a3c5b5e7509
Rautenberg, Emily <Emily_Rautenberg@comcast.com>
2022-10-03 11:38:17 -0400

    fix(Theme): update Rogers colors (#1091)

--- themes/rogers/color.js
+++ themes/rogers/color.js
@@ -1,20 +1,24 @@
 import ALPHA from '../base/alpha';
+import { convertHexToAlphaArray } from '../utils';
 
-const COLOR_PALETTE = {
-  red: {
-    20: '#D40139',
-    40: '#A1001E',
-    90: '38060E'
-  }
+const RED = {
+  'red-20': '#D40139',
+  'red-40': '#A1001E',
+  'red-90': '38060E'
 };
 
 export default {
-  materialBrand: [COLOR_PALETTE.red[90], ALPHA.primary],
-  coreBrand: [COLOR_PALETTE.red[40], ALPHA.primary],
-  coreBrandSecondary: [COLOR_PALETTE.red[40], ALPHA.secondary],
-  coreBrandTertiary: [COLOR_PALETTE.red[40], ALPHA.tertiary],
-  coreBrandDisabled: [COLOR_PALETTE.red[40], ALPHA.inactive],
-  interactiveBrandFocus: [COLOR_PALETTE.red[20], ALPHA.primary],
-  interactiveBrandFocusSoft: [COLOR_PALETTE.red[20], ALPHA.tertiary],
-  red: [COLOR_PALETTE.red[40], ALPHA.primary]
+  red: [RED['red-40'], ALPHA.primary],
+  ...convertHexToAlphaArray(RED, ALPHA.primary),
+  materialBrand: [RED['red-90'], ALPHA.primary],
+  fillBrand: [RED['red-40'], ALPHA.primary],
+  fillBrandSecondary: [RED['red-40'], ALPHA.secondary],
+  fillBrandTertiary: [RED['red-40'], ALPHA.tertiary],
+  fillBrandDisabled: [RED['red-40'], ALPHA.inactive],
+  strokeBrand: [RED['red-40'], ALPHA.primary],
+  strokeBrandSecondary: [RED['red-40'], ALPHA.secondary],
+  strokeBrandTertiary: [RED['red-40'], ALPHA.tertiary],
+  strokeBrandDisabled: [RED['red-40'], ALPHA.inactive],
+  interactiveBrandFocus: [RED['red-20'], ALPHA.primary],
+  interactiveBrandFocusSoft: [RED['red-20'], ALPHA.tertiary]
 };
