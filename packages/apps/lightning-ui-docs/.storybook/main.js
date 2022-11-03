159a69f9f3db4a2d004d4dc638b200b7fc5d78f9
Emily Rautenberg <erautenb@gmail.com>
2022-11-03 16:26:10 -0400

    fix(Storybook): update paths for docs and props table (#1150)
    * fix(Scripts): update theming prop table generation scripts
    
    * fix(Docs): update paths for docs to show in Storybook
    
    * fix(Docs): update import paths
    
    Co-authored-by: Richards, David (Contractor) <David_Richards3@comcast.com>

--- packages/apps/lightning-ui-docs/.storybook/main.js
+++ packages/apps/lightning-ui-docs/.storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
   stories: [
     '../../../@lightning/ui/src/**/*.stories.@(js|jsx|ts|tsx)',
     '../../../@lightning/ui-core/src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../@lightning/ui-core/docs/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   staticDirs: ['../../../@lightning/ui-core/src/assets'], // TODO: How to handle images between projects
   core: {
---
+++ packages/apps/lightning-ui-docs/scripts/get-storybook-links.mjs
@@ -0,0 +1,46 @@
+import LineReaderSync from 'line-reader-sync';
+import fs from 'fs';
+
+/**
+ * Finds all the elements and patterns and what style value they are using
+ * @param {string} component - name of the actual component
+ * @param {string} path - file path to get to the component
+ * @param {string} name - name of the parent component in case the child component does not have a .mdx file
+ * @returns {string} holds the url the user would navigate to when a component is clicked
+ */
+export function getStorybookLinks(component, path, name) {
+  // get mdx paths
+  const originalPath = `./${path}/${component}.mdx`;
+  let modifiedPath = originalPath;
+  if (component != name) {
+    // this is incase there are multiple elements nested inside of one folder
+    modifiedPath = `./${path}/${name}.mdx`;
+    if (!fs.existsSync(modifiedPath)) {
+      modifiedPath = originalPath; // this is in case there are multiple elements nested inside of one folder but not all of them have .mdx files
+    }
+  }
+  if (!fs.existsSync(modifiedPath)) {
+    // this is if even after all the checks a file is formated weird or does not exist
+    return;
+  }
+  const lrs = new LineReaderSync(modifiedPath);
+  const storyIdString = '<Story id'; // starting point of the line we're searching for
+  const urlStart = '../?path=/docs/'; // beginning url of component's storybook documentation page
+  while (true) {
+    // start searching for the storybook url line
+    const line = lrs.readline();
+    if (line.includes(storyIdString)) {
+      const firstQuotation = line.indexOf('"');
+      const firstSubstring = line.substring(
+        firstQuotation + 1,
+        line.length - 1
+      );
+      const removeCharacters = firstSubstring
+        .replace(' ', '')
+        .replace('"', '')
+        .replace('/', '');
+      const url = urlStart.concat(removeCharacters);
+      return url;
+    }
+  }
+}
