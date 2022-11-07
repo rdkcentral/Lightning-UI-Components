20a1a7226f18e031399a59a1e8feb0fad3a97810
Yendluri, Ranjitha Lakshmi (Contractor) <RanjithaLakshmi_Yendluri@comcast.com>
2022-11-07 10:03:46 -0500

    test: tile basic automation (#1138)
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic Automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: fix merge issues
    
    * test: test basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    * test: tile basic automation
    
    Co-authored-by: skvits675 <sergii_kvitsynskyi@comcast.com>
    Co-authored-by: Sachidanandam, Balaji (Contractor) <Balaji_Sachidanandam@comcast.com>

---
+++ cypress/e2e/step-definitions/Tile.js
@@ -0,0 +1,71 @@
+import Tile from '../pageObjects/tilesAndCards/tile.tilesandcards';
+
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+
+export default function () {
+
+  /**
+   * @module Tile
+   * @function I verify that the {string} is {string} for {string} component
+   * @description Cucumber statement to verify the property of a component
+   * @param {String} property - property
+   * @param {String} value - value
+   * @param {String} component - component
+   * @example I verify that the 'persistentMetadata' is 'true' for 'Tile' component
+   */
+  Then(
+    'I verify that the {string} is {string} for {string} component',
+    (prop, value, component) => {
+
+      switch (prop) {
+        case "persistentMetadata":
+          if (value === "true") {
+            cy.get(Tile.metadata).should('not.have.attr', 'alpha');
+          } else {
+            cy.get(Tile.metadata).should('have.attr', 'alpha');
+          }
+          break;
+        case "blur":
+          if (value === "true") {
+            cy.get(Tile.blur).should('have.attr', 'alpha');
+          } else {
+            cy.get(Tile.blur).should('not.have.attr', 'alpha');
+          }
+          break;
+        case "fill":
+          if (value === "true") {
+            cy.get(Tile.fill).should('be.visible');
+          } else {
+            cy.get(Tile.fill).should('not.exist');
+          }
+          break;
+        case "checked":
+          if (value === "true") {
+            cy.get(Tile.checked).should('be.visible');
+          } else {
+            cy.get(Tile.checked).should('not.exist');
+          }
+          break;
+        case "standard":
+          cy.get(Tile.metadata).should('not.have.attr', 'mounty');
+          break;
+        case "inset":
+          cy.get(Tile.metadata).should('have.attr', 'mounty');
+          break;
+        case 'default':
+          cy.get(Tile.format).should('not.have.attr', 'alpha');
+          break;
+        case 'circle':
+          cy.get(Tile.format).should('have.attr', 'alpha', '0.001');
+          break;
+        case 'square':
+          cy.get(Tile.format).should('have.attr', 'alpha', '0.001');
+          break;
+        case 'contain':
+          cy.get(Tile.format).should('not.have.attr', 'alpha');
+          break;
+        default:
+          break;
+      }
+    });
+}
