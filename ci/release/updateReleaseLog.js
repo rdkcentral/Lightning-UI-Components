94e1b30d0db577c1ab18de4b47170a7b4581a3ae
Josh Howenstine <joshua_howenstine@comcast.com>
2022-11-09 16:32:08 -0800

    ci: add [skip deploy] guards

--- ci/release/updateReleaseLog.js
+++ ci/release/updateReleaseLog.js
@@ -17,7 +17,7 @@ exec('git rev-parse HEAD', (err, hash) => {
       }
       console.log('Release log updated')
       exec(
-        `git add ./ci/release/.release.json && git commit -m 'ci: update release log [ci skip]'`, // Make sure this commit does not retrigger the release pipeline
+        `git add ./ci/release/.release.json && git commit -m 'ci: update release log [skip deploy]'`, // Make sure this commit does not retrigger the release pipeline
         (err, output) => {
           if (err) {
             console.error(output);
