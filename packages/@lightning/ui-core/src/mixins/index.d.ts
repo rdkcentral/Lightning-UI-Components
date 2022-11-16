76661c4d100acce694cf2b048ce3f18f2552b71f
Josh Howenstine <Joshhowenstine@gmail.com>
2022-11-16 08:44:01 -0800

    fix(TypeScript): add definitions for mixins (#1202)

--- packages/@lightning/ui-core/src/mixins/index.d.ts
+++ packages/@lightning/ui-core/src/mixins/index.d.ts
@@ -3,16 +3,16 @@ export {
   generateAbbrevConfig,
   defaultAbbrevConfig
 } from './withAnnouncer';
-export { default as withClassCache } from './withClassCache';
+// TODO: Add withClassCache
+// TODO: Add withEditItems
+// TODO: Fix definition export { default as withExtensions } from './withExtensions';
 export { default as withHandleKey } from './withHandleKey';
 export { default as withLayout, LayoutOptions } from './withLayout';
+export { default as withMarqueeSync } from './withMarqueeSync';
 export { default as withSelections } from './withSelections';
+export { default as withTags } from './withTags';
 export {
   default as withThemeStyles,
   processThemeStyles
 } from './withThemeStyles';
-export { default as withTags } from './withTags';
-export { default as withTransitions } from './withTransitions';
 export { default as withUpdates } from './withUpdates';
-export { default as withExtensions } from './withExtensions';
-export { default as withMarqueeSync } from './withMarqueeSync';
