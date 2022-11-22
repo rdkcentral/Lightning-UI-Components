fc2a3e39492c1a40f932e53e49e8825016d758ef
Wang, Jasmine <Jasmine_Wang@comcast.com>
2022-11-22 15:11:46 -0500

    fix(Storybook): reorder doc stories in docs section (#1223)

--- packages/apps/lightning-ui-docs/.storybook/preview.js
+++ packages/apps/lightning-ui-docs/.storybook/preview.js
@@ -103,7 +103,7 @@ export const parameters = {
   options: {
     storySort: {
       method: 'alphabetical',
-      order: [CATEGORIES[1024], CATEGORIES[2048], CATEGORIES[0], CATEGORIES[4], CATEGORIES[512], CATEGORIES[64], CATEGORIES[16], CATEGORIES[2], CATEGORIES[8], CATEGORIES[256], CATEGORIES[32], CATEGORIES[128]]
+      order: [CATEGORIES[1024], ['Introduction', 'Read Me', 'Contributing', 'Lightning Resources', 'Theming', ['Overview', 'Theme Properties', 'Use in Storybook', 'Mode', 'Palette', 'Extensions', 'Sub Theming', '*']], CATEGORIES[2048], CATEGORIES[0], CATEGORIES[4], CATEGORIES[512], CATEGORIES[64], CATEGORIES[16], CATEGORIES[2], CATEGORIES[8], CATEGORIES[256], CATEGORIES[32], CATEGORIES[128]]
     }
   }
 };
