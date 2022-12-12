74fd5da03c9f4a94107b1c62074e6619249165ce
Yendluri, Ranjitha Lakshmi (Contractor) <RanjithaLakshmi_Yendluri@comcast.com>
2022-12-12 08:57:10 -0500

    test: checkbox Automation (#1279)
    * test: checkbox automation
    
    * test: checkbox automation
    
    * test: checkbox automation
    
    * test: checkbox automation
    
    * test: checkbox automation
    
    * test: checkbox automation
    
    * test: checkbox automation
    
    Co-authored-by: Radhakrishnan, Rajanika <Rajanika_Radhakrishnan@comcast.com>

---
+++ cypress/e2e/pageObjects/utilities/checkbox.utilities.js
@@ -0,0 +1,17 @@
+import LUIComponent from '../component';
+
+class Checkbox extends LUIComponent {
+  constructor() {
+    super({ type: 'utilities', componentName: 'checkbox' });
+  }
+
+  get checkbox() {
+    return 'div[type=checkbox]';
+  }
+
+  get checked(){
+    return 'div[type=checkbox]>div[type=icon]'
+  }
+}
+
+module.exports = new Checkbox();
