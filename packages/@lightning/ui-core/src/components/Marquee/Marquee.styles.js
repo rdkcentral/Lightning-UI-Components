b833a382c010bbc227303e56083f5fe0086efb3c
Rautenberg, Emily <Emily_Rautenberg@comcast.com>
2022-12-15 15:40:03 -0500

    fix(Marquee): do not set text alignment in default text style (#1305)
    * fix(Marquee): do not set text alignment in default text style
    
    * test: update snapshot

--- packages/@lightning/ui-core/src/components/Marquee/Marquee.styles.js
+++ packages/@lightning/ui-core/src/components/Marquee/Marquee.styles.js
@@ -2,8 +2,5 @@ export const base = theme => ({
   fadeW: theme.layout.gutterX.lg,
   offset: theme.spacer.xxl,
   shouldSmooth: false,
-  textStyle: {
-    ...theme.typography.body1,
-    textAlign: 'center'
-  }
+  textStyle: theme.typography.body1
 });
