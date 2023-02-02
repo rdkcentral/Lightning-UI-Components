260fb056e7ecd0245e5e2760df11fb8c66c1ed63
Salhan, Anita <ANITA_SALHAN@comcast.com>
2023-02-02 10:43:19 -0500

    fix: switch fillNeutral to textNeutral (#1416)
    * fix/fillNeutral-mapped- to-textNeutral
    
    * fix(GridOverlay): update to textNeutral
    
    * Trigger Concourse
    
    * chore(Gridoverlay): fix format

--- packages/@lightning/ui-core/src/components/Badge/Badge.styles.js
+++ packages/@lightning/ui-core/src/components/Badge/Badge.styles.js
@@ -12,16 +12,16 @@ export const tone = theme => ({
   neutral: {
     backgroundColor: theme.color.fillInverseSecondary,
     iconColor: theme.color.fillNeutral,
-    textStyle: { textColor: theme.color.fillNeutral }
+    textStyle: { textColor: theme.color.textNeutral }
   },
   inverse: {
     backgroundColor: theme.color.fillNeutralSecondary,
     iconColor: theme.color.fillInverse,
-    textStyle: { textColor: theme.color.fillInverse }
+    textStyle: { textColor: theme.color.textInverse }
   },
   brand: {
     backgroundColor: theme.color.fillBrand,
     iconColor: theme.color.fillNeutral,
-    textStyle: { textColor: theme.color.fillNeutral }
+    textStyle: { textColor: theme.color.textNeutral }
   }
 });
--- packages/@lightning/ui-core/src/components/GridOverlay/GridOverlay.styles.js
+++ packages/@lightning/ui-core/src/components/GridOverlay/GridOverlay.styles.js
@@ -1,7 +1,10 @@
 export const base = theme => ({
   propertyTextStyle: {
     ...theme.typography.headline1,
-    textColor: theme.color.white
+    textColor: theme.color.textNeutral
   },
-  valueTextStyle: { ...theme.typography.body1, textColor: theme.color.white }
+  valueTextStyle: {
+    ...theme.typography.body1,
+    textColor: theme.color.textNeutral
+  }
 });
