eba64513ae7d8be63ae85ed9e31b74d7085428ae
Howenstine, Joshua <Joshua_Howenstine@comcast.com>
2023-02-15 14:11:29 -0800

    fix: add correct background color to match color of logo (#1466)
    * fix: add correct background color to match color of logo
    
    * Trigger Concourse

--- packages/apps/lightning-ui-docs/.storybook/theme.js
+++ packages/apps/lightning-ui-docs/.storybook/theme.js
@@ -4,6 +4,7 @@ import { version } from '@lightning/ui/package.json';
 
 export default create({
   base: 'dark',
+  appBg: 'rgba(33, 35, 43, 1)',
   textColor: 'white',
   brandTitle: `Lightning UI Components v${version}`,
   brandUrl: 'https://github.comcast.com/Lightning/lightning-ui',
