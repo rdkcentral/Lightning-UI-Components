a6d218648a167eb4b2dafad4084d3e3832d4bd22
Anthony Gallo <anthony.gallo@hey.com>
2023-02-16 15:35:26 -0500

    feat: update @lightningjs/core peerDependency to 2.9.0 (#1479)
    * update @lightningjs/core peerDependency to 2.9.0 across all packages
    * normalize peerDependency location
    * remove @lightningjs/core as a standard dependency from all @lightning packages
    * remove @lightningjs/core resolve alias from storybook webpack config

--- packages/apps/lightning-ui-docs/.storybook/webpack.config.js
+++ packages/apps/lightning-ui-docs/.storybook/webpack.config.js
@@ -1,10 +1,6 @@
 const path = require('path');
 
 module.exports = async ({ config, mode }) => {
-  config.resolve.alias['@lightningjs/core'] = path.resolve(
-    __dirname,
-    '../node_modules/@lightningjs/core'
-  ); // Make sure both storybook and lightning/ui components use the same instance of @lightingjs/core otherwise you will get shader errors
   config.resolve.alias['@lightning-inspect'] =
     mode === 'PRODUCTION'
       ? path.resolve(__dirname, '../lightning-inspect/production')
