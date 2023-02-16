8ac58fa36102965cc23100e62bfac0c8e2ffe8df
Soumya Loka <lokareddy222@gmail.com>
2023-02-16 18:15:15 -0500

    fix(Knob): update width and height values in styles (#1476)
    Co-authored-by: soumya <soumyaloka@comcast.com>

--- packages/@lightning/ui-core/src/components/Toggle/ToggleSmall.styles.js
+++ packages/@lightning/ui-core/src/components/Toggle/ToggleSmall.styles.js
@@ -1,11 +1,11 @@
 export const base = theme => ({
-  w: theme.layout.gutterX.sm,
+  h: theme.spacer.md * 3,
   strokeRadius: theme.radius.md * 0.75,
   strokeWeight: theme.stroke.sm,
   knobWidth: theme.spacer.lg,
   knobHeight: theme.spacer.lg,
   knobRadius: theme.radius.sm,
   knobPadding: theme.spacer.xs,
-  knobCheckedX: theme.spacer.xl + theme.spacer.xs,
-  knobUncheckedX: theme.spacer.sm
+  knobXChecked: theme.spacer.xl + theme.spacer.xs,
+  w: theme.spacer.md * 6
 });
