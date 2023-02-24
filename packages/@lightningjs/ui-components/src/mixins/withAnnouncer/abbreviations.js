a845682e2e47a7e057213ec35c4d35e12f593aa1
Emily Rautenberg <erautenb@gmail.com>
2023-02-24 14:51:38 -0500

    chore: add License and Copyright to all JS files

--- packages/@lightningjs/ui-components/src/mixins/withAnnouncer/abbreviations.js
+++ packages/@lightningjs/ui-components/src/mixins/withAnnouncer/abbreviations.js
@@ -1,3 +1,21 @@
+﻿/**
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 // Don't wrap patterns with word boundaries (\b) or set any RegExp flags.
 // Each pattern will be wrapped with \b and use the global (g) flag when created as a capture group in combined pattern.
 const defaultAbbreviations = [
