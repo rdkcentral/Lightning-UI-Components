be1c85a47a38ee8e0d42115f7b1f31dcedf02074
Emily Rautenberg <erautenb@gmail.com>
2023-02-24 14:47:04 -0500

    fix: update generate theme props table to import JS instead of JSON

--- packages/apps/lightning-ui-docs/scripts/generate-theme-props-table.mjs
+++ packages/apps/lightning-ui-docs/scripts/generate-theme-props-table.mjs
@@ -7,7 +7,7 @@ const require = createRequire(import.meta.url);
 const colorJsonFile = require('./roles/colorRole.json');
 const typographyJsonFile = require('./roles/typographyRole.json');
 
-const base = require('@lightningjs/ui-base-theme');
+import base from '@lightningjs/ui-base-theme';
 const { radius, typography, color } = base;
 
 const folderPath = 'scripts/themePropertyTables';
