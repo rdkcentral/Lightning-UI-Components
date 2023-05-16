a87a0d3ab36ac516c3aa3e515aa70ac4bc81d7c9
Anthony Gallo <anthony_gallo2@comcast.com>
2023-05-16 11:47:57 -0400

    feat(FocusManager): update declaration to extend Base correctly

--- packages/@lightningjs/ui-components/src/components/FocusManager/FocusManager.d.ts
+++ packages/@lightningjs/ui-components/src/components/FocusManager/FocusManager.d.ts
@@ -19,44 +19,129 @@
 import lng from '@lightningjs/core';
 import Base from '../Base';
 
-export default class FocusManager extends Base {
-  protected _selectedChange(
-    selected: lng.Component | undefined,
-    prevSelected: lng.Component | undefined
-  ): void;
-  protected _selectedIndex: number;
-
-  direction?: string;
-  set items(
+export type NavigationDirectionType = 'none' | 'column' | 'row';
+export type FocusItemsType = Array<
+  lng.Component.NewPatchTemplate<lng.Component.Constructor> | lng.Component
+>;
+declare namespace FocusManager {
+  export interface TemplateSpec extends Base.TemplateSpec {
+    /**
+     * the navigation direction
+     */
+    direction?: NavigationDirectionType;
+    /**
+     * child element or elements of the FocusManager
+     */
     items: Array<
-      lng.Component.NewPatchTemplate<lng.Component.Constructor> | lng.Component
-    >
-  );
-  get items(): Array<
-    lng.Component.NewPatchTemplate<lng.Component.Constructor> | lng.Component
-  >;
+      | lng.Component.NewPatchTemplate<lng.Component.Constructor>
+      | typeof lng.Component
+    >;
+    /**
+     * index of currently selected item
+     */
+    selectedIndex?: number;
+
+    /**
+     * enables wrapping behavior, so `selectNext` selects the first item if the current item is the last on the list and vice versa
+     */
+    wrapSelected?: boolean;
+  }
+}
+
+declare class FocusManager<
+  TemplateSpec extends FocusManager.TemplateSpec,
+  TypeConfig extends lng.Component.TypeConfig
+> extends Base<TemplateSpec, TypeConfig> {
+  // Properties
+
+  /**
+   * the navigation direction
+   */
+  direction?: NavigationDirectionType;
+
+  /**
+   * child element or elements of the FocusManager
+   */
+  items: FocusItemsType;
+
+  /**
+   * index of currently selected item
+   */
+  selectedIndex: number;
+
+  /**
+   * enables wrapping behavior, so `selectNext` selects the first item if the current item is the last on the list and vice versa
+   */
+  wrapSelected: boolean;
+
+  // Accessors
+
   get Items(): lng.Element;
 
+  /**
+   * returns the currently selected component
+   */
   get selected(): lng.Component;
-  get selectedIndex(): number;
-  set selectedIndex(index: number);
+
+  /**
+   * returns an array containing the children of the FocusManager that are fully within the visible bounds of the FocusManager
+   */
   get fullyOnScreenItems(): Array<lng.Component>;
 
-  appendItems(
-    items: Array<lng.Component.NewPatchTemplate<lng.Component.Constructor>>
-  ): void;
-  appendItemsAt(
-    items: Array<lng.Component.NewPatchTemplate<lng.Component.Constructor>>,
-    idx: number
-  ): void;
-  prependItems(
-    items: Array<lng.Component.NewPatchTemplate<lng.Component.Constructor>>
-  ): void;
+  // Methods
+
+  /**
+   * adds the provided Rows to the end of the FocusManager children
+   * @param items array of Rows or Row-type patch objects
+   */
+  appendItems(items: FocusItemsType): void;
+
+  /**
+   * adds the provided Rows to the end of the FocusManager children at the provided index
+   * @param items array of Rows or Row-type patch objects
+   * @param idx index to insert rows at
+   */
+  appendItemsAt(items: FocusItemsType, idx: number): void;
+
+  /**
+   * adds the provided Rows to the beginning of the FocusManager children
+   * @param items array of Rows or Row-type patch objects
+   */
+  prependItems(items: FocusItemsType): void;
+
+  /**
+   * removes Item from FocusManager children at the given index
+   * @param index index of row to remove
+   */
   removeItemAt(idx: number): void;
+
+  /**
+   * returns the transition value for `x` of FocusManager.Items
+   */
+  getTransitionXTargetValue(): string;
+
+  /**
+   * returns the transition value for `y` of FocusManager.Items
+   */
+  getTransitionYTargetValue(): string;
+
+  /**
+   * A no-op function that is called when `selectedIndex` is set. Can be overridden by classes that extend `FocusManager` for custom render behavior.
+   */
   render(): void;
+
+  /**
+   * Selects previous item. If this.wrapSelected=true, will select the last element in the list if focus is currently on the first item.
+   */
   selectPrevious(): void;
+
+  /**
+   * Selects next item. If this.wrapSelected=true, will select the first element in the list if focus is currently on the last item.
+   */
   selectNext(): void;
 
   // tags
   get _Items(): lng.Component;
 }
+
+export default FocusManager;
