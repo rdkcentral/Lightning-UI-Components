e882a98b167cf0106c290cc7a64bc5c7d9375a95
jwang851 <jasmine_wang@comcast.com>
2023-05-31 03:51:50 -0400

    chore(NavigationManager): add comments to each property/method

--- packages/@lightningjs/ui-components/src/components/NavigationManager/NavigationManager.d.ts
+++ packages/@lightningjs/ui-components/src/components/NavigationManager/NavigationManager.d.ts
@@ -44,12 +44,45 @@ export type NavigationManagerStyles = {
 
 declare namespace NavigationManager {
   export interface TemplateSpec extends FocusManager.TemplateSpec {
+    /**
+     * Determines whether the component will stop scrolling as it nears the end to prevent white space.
+     * If true, the component will scroll infinitely.
+     */
     alwaysScroll?: boolean;
+
+    /**
+     * If true, the component will never scroll, unless `alwaysScroll` is set to true.
+     * If false, the component will apply normal scrolling logic.
+     */
     neverScroll?: boolean;
+
+    /**
+     * Item index at which scrolling begins, provided the sum of item widths is greater than the width of the Row component.
+     */
     scrollIndex?: number;
+
+    /**
+     * Automatically sets the width of the row to be the same as the Items container inside (this will prevent scrolling).
+     */
     autoResizeWidth?: boolean;
+
+    /**
+     * Automatically sets the height of the row to be the same as the Items container inside.
+     */
     autoResizeHeight?: boolean;
+
+    /**
+     * If set, this is used to calculate the initial number of items to display on the first render (`lazyUpCount` + `lazyLoadUpCountBuffer`).
+     * The remaining items are stored as lazy items.
+     * Each time the user navigates further, the next lazy item will be added.
+     */
     lazyUpCount?: number;
+
+    /**
+     * If `lazyUpCount` is set, this is used to calculate the initial number of items to display on the first render (`lazyUpCount` + `lazyLoadUpCountBuffer`).
+     * The remaining items are stored as lazy items.
+     * Each time the user navigates further, the next lazy item will be added.
+     */
     lazyUpCountBuffer?: number;
   }
 }
@@ -59,12 +92,45 @@ declare class NavigationManager<
   TypeConfig extends lng.Component.TypeConfig = lng.Component.TypeConfig
 > extends FocusManager<TemplateSpec, TypeConfig> {
   // Properties
+  /**
+   * Determines whether the component will stop scrolling as it nears the end to prevent white space.
+   * If true, the component will scroll infinitely.
+   */
   alwaysScroll?: boolean;
+
+  /**
+   * If true, the component will never scroll, unless alwaysScroll is set to true.
+   * If false, the component will apply normal scrolling logic.
+   */
   neverScroll?: boolean;
+
+  /**
+   * Item index at which scrolling begins, provided the sum of item widths is greater than the width of the Row component.
+   */
   scrollIndex?: number;
+
+  /**
+   * Automatically sets the width of the row to be the same as the Items container inside (this will prevent scrolling).
+   */
   autoResizeWidth?: boolean;
+
+  /**
+   * Automatically sets the height of the row to be the same as the Items container inside.
+   */
   autoResizeHeight?: boolean;
+
+  /**
+   * If set, this is used to calculate the initial number of items to display on the first render (`lazyUpCount` + `lazyLoadUpCountBuffer`).
+   * The remaining items are stored as lazy items.
+   * Each time the user navigates further, the next lazy item will be added.
+   */
   lazyUpCount?: number;
+
+  /**
+   * If `lazyUpCount` is set, this is used to calculate the initial number of items to display on the first render (`lazyUpCount` + `lazyLoadUpCountBuffer`).
+   * The remaining items are stored as lazy items.
+   * Each time the user navigates further, the next lazy item will be added.
+   */
   lazyUpCountBuffer?: number;
 
   // Accessors
@@ -79,13 +145,55 @@ declare class NavigationManager<
   set style(v: StylePartial<NavigationManagerStyles>);
 
   // Methods
+  /**
+   * An event that, when triggered, calls a method that forces the component to update.
+   */
   $itemChanged(): void;
+
+  /**
+   * Updates the position of a component along the main axis of the NavigationManager component.
+   * For components where `direction` is `'row'`, it will update the `x` property on the passed in component(`item`) to the passed in value(`position`).
+   * For components where `direction` is `'column'`, it will update the `y` property on the passed in component(`item`) to the passed in value(`position`).
+   * @param item component whose property will be updated
+   * @param position value to replace either the `x` or `y` property of item
+   */
   updatePositionOnAxis(item: lng.Component, position: number): void;
+
+  /**
+   * Scrolls to the child at the supplied index at the rate of `duration` * (`this.selectedIndex` - `index`)
+   * @param index index of item to scroll to
+   * @param duration how long, in ms, it should take to scroll to the item at index
+   */
   scrollTo(index: number, duration: number): void;
+
+  /**
+   * A method that is invoked upon any transitions to the component's axis property finishing (ex. 'x' for Row, 'y' for Column).
+   * It is not set by default and can be overwritten.
+   */
   transitionDone(): void;
+
+  /**
+   * Returns a boolean for whether or not the component should scroll left
+   * based off if the component's `direction` property is `'row'`, the selected item's index is past the `scrollIndex`, and that content that is out of view can be scrolled to.
+   */
   shouldScrollLeft(): boolean;
+
+  /**
+   * Returns a boolean for whether or not the component should scroll right
+   * based off if the component's `direction` property is `'row'`, the selected item's index is past the `scrollIndex`, and that content that is out of view can be scrolled to.
+   */
   shouldScrollRight(): boolean;
+
+  /**
+   * Returns a boolean for whether or not the component should scroll up
+   * based off if the component's `direction` property is `'column'`, the selected item's index is past the `scrollIndex`, and that content that is out of view can be scrolled to.
+   */
   shouldScrollUp(): boolean;
+
+  /**
+   * Returns a boolean for whether or not the component should scroll down
+   * based off if the component's `direction` property is `'column'`, the selected item's index is past the `scrollIndex`, and that content that is out of view can be scrolled to.
+   */
   shouldScrollDown(): boolean;
 }
 
