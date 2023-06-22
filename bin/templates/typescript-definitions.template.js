f625fc6f259150bea20dc0e7439aa73c91130b51
Jasmine Wang <44105951+jazzyw@users.noreply.github.com>
2023-06-22 10:19:18 -0400

    fix(Template): update TS template to reflect new structure (#252)
    * fix(Template): update TS template to reflect new structure
    
    * chore(Template): add License and Copyright to new components
    
    * fix: ensure import is pointing to correct path

--- bin/templates/typescript-definitions.template.js
+++ bin/templates/typescript-definitions.template.js
@@ -17,20 +17,50 @@
  */
 
 module.exports = name => {
-  return `import type { Base, StylePartial } from '../../types/lui';
+  return `/**
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-export type ${name}Style = {
+import lng from '@lightningjs/core';
+import Base from '../Base/Base'; // TODO: remove this comment or replace import with appropriate parent component import
+import type { StylePartial } from '../../types/lui';
 
-};
+export type ${name}Style = {};
 
-export default class ${name} extends Base {
-  //add public class methods here
+declare namespace ${name} {
+  export interface TemplateSpec extends Base.TemplateSpec {
+    // include properties from the below class declaration and anything that can be patched to the component
+  }
+}
+
+declare class ${name}<
+  TemplateSpec extends ${name}.TemplateSpec = ${name}.TemplateSpec,
+  TypeConfig extends lng.Component.TypeConfig = lng.Component.TypeConfig
+> extends Base<TemplateSpec, TypeConfig> {
+  // Properties
+
+  // Accessors
   get style(): ${name}Style;
   set style(v: StylePartial<${name}Style>);
 
+  // Methods
 
-  //add all component tags here
+  // Tags
 }
+
+export default ${name};
 `}
-
-
