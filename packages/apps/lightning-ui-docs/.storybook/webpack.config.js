182c0f7471d2383b218d3609a13e3cf20337cbfa
Mandy Wehrman <arwehrman@gmail.com>
2023-06-23 13:00:38 -0400

    feat(Storybook): updates Storybook to use webpack5

--- packages/apps/lightning-ui-docs/.storybook/webpack.config.js
+++ packages/apps/lightning-ui-docs/.storybook/webpack.config.js
@@ -20,10 +20,10 @@ const path = require('path');
 
 module.exports = async ({ config, mode }) => {
   config.optimization.minimize = false; // Minification seams to to break FocusManager navigation
-    // Shorter alias for inspector
-    config.resolve.alias['lightningInspect'] = path.resolve(
-      __dirname,
-      '../../../../node_modules/@lightningjs/core/devtools/lightning-inspect'
-    );
+  // Shorter alias for inspector
+  config.resolve.alias['lightningInspect'] = path.resolve(
+    __dirname,
+    '../../../../node_modules/@lightningjs/core/devtools/lightning-inspect'
+  );
   return config;
 };
