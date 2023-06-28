e6a8d22eae2bd49f8a9e62db3d246b63f38681b1
Loka <lokareddy222@gmail.com>
2023-06-28 16:43:34 -0400

    fix: update logic

--- packages/@lightningjs/ui-components/src/components/InlineContent/InlineContent.js
+++ packages/@lightningjs/ui-components/src/components/InlineContent/InlineContent.js
@@ -61,12 +61,12 @@ export default class InlineContent extends Base {
   }
 
   _updateContent() {
-    this.childList.clear();
+   this.childList.clear();
 
     // if wrapping with max lines, hide content until it has rerendered with maxLines and truncation calculated
-if (this._shouldTruncate) {
+     if (this._shouldTruncate) {
       this.alpha = 0.001;
-    }
+    }      
 
     if (this._parsedContent && this._parsedContent.length) {
       this.patch({
