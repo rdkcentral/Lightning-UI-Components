086b48e9e5a78bdd6f812a6f16299e31340845a8
Josh Howenstine <joshhowenstine@gmail.com>
2023-07-25 07:18:17 -0700

    fix: change postpack script to rename package.json to restore all scripts

--- scripts/postpack.js
+++ scripts/postpack.js
@@ -1,111 +1,39 @@
 const fs = require('fs');
 const path = require('path');
-const zlib = require('zlib');
-const tar = require('tar');
 
-const { execSync } = require('child_process');
+// Function to rename 'package-bck.json' to 'package.json'
+const renamePackageBckToJson = (directoryPath) => {
+  const packageBckPath = path.join(directoryPath, 'package-bck.json');
+  const packageJsonPath = path.join(directoryPath, 'package.json');
 
-// Helper function to replace @ and / characters with -
-const replaceCharacters = str => {
-  const replacedStr = str.replace(/[@/]/g, '-');
-  return replacedStr.startsWith('-') ? replacedStr.slice(1) : replacedStr;
-};
+  fs.access(packageBckPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error('Error: package-bck.json does not exist in the specified directory.');
+      return;
+    }
 
-// Get the package name from environment variables
-const packageName = process.env.packageName;
-console.log('Passed Package Name:', packageName);
-
-// Get the workspace information for the specified package using yarn
-const workspacesInfoOutput = execSync('yarn workspaces list --json').toString();
-const workspaces = JSON.parse(
-  '[' + workspacesInfoOutput.replace(/\n/g, ',').replace(/,\s*$/, '') + ']'
-);
-const { name: workspaceName, location: workspaceLocation } = workspaces.find(
-  ({ name }) => name === packageName
-);
-
-const tgzFilePath = path.resolve(workspaceLocation, 'package.tgz')
-
-// Step 1: Unzip the .tgz file and extract its contents to a folder
-const extractFolder = path.resolve(workspaceLocation, 'extracted');
-fs.mkdirSync(extractFolder, { recursive: true });
-
-const readStream = fs.createReadStream(tgzFilePath);
-const extractOptions = { cwd: extractFolder, strip: 1 };
-
-readStream.pipe(zlib.createGunzip()).pipe(tar.extract(extractOptions))
-  .on('finish', () => {
-    console.log('Unzipping and extracting completed.');
-
-    // Step 2: Modify the desired fields in package.json
-    const packageJsonPath = path.join(extractFolder, 'package.json');
-
-    const originalPackageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-
-    const desiredFields = [
-      'name',
-      'version',
-      'description',
-      'dependencies',
-      'files',
-      'homepage',
-      'sideEffects',
-      'type',
-      'peerDependencies',
-      'publishConfig',
-      'repository',
-      'exports',
-      'main',
-      'types'
-    ];
-
-    const newPackageJson = Object.fromEntries(
-      Object.entries(originalPackageJson).filter(([key]) => desiredFields.includes(key))
-    );
-
-    // Remove the 'scripts' property from the package.json
-    delete newPackageJson.scripts;
-
-    // Step 3: Write the modified content to the new package.json
-    const newPackageJsonPath = path.join(extractFolder, 'package.json');
-    fs.writeFileSync(newPackageJsonPath, JSON.stringify(newPackageJson, null, 2));
-
-    console.log('package.json updated.');
-
-    // Step 4: Create a new tarball with the modified content
-    const modifiedTgzFilePath = path.resolve(workspaceLocation, 'package.tgz');
-    const writeStream = fs.createWriteStream(modifiedTgzFilePath);
-
-    const packOptions = { gzip: true, cwd: extractFolder };
-    tar.c(packOptions, ['.'])
-      .pipe(writeStream)
-      .on('finish', () => {
-        console.log('Modified content zipped back to a new .tgz file.');
-
-        // Step 5: Remove the extracted folder
-        removeDirRecursive(extractFolder);
-        console.log('Extracted folder removed.');
-
-        // Step 6: Remove the dist folder (if it exists)
-        const distFolderPath = path.resolve(workspaceLocation, 'dist');
-        if (fs.existsSync(distFolderPath)) {
-          removeDirRecursive(distFolderPath);
-          console.log('dist folder removed.');
-        }
-      });
-  });
-
-// Helper function to remove directories recursively
-function removeDirRecursive(dirPath) {
-  if (fs.existsSync(dirPath)) {
-    fs.readdirSync(dirPath).forEach((file) => {
-      const curPath = path.join(dirPath, file);
-      if (fs.lstatSync(curPath).isDirectory()) {
-        removeDirRecursive(curPath);
+    fs.rename(packageBckPath, packageJsonPath, (err) => {
+      if (err) {
+        console.error('Error renaming package-bck.json to package.json:', err.message);
       } else {
-        fs.unlinkSync(curPath);
+        console.log('package-bck.json renamed to package.json successfully!');
       }
     });
-    fs.rmdirSync(dirPath);
-  }
+  });
+};
+
+// Check if packageName environment variable is set
+if (!process.env.packageName) {
+  console.error('Error: packageName environment variable is not set.');
+} else {
+  const directoryPath = path.resolve(process.env.packageName);
+
+  // Check if the specified directory exists
+  fs.access(directoryPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error('Error: Specified directory does not exist.');
+    } else {
+      renamePackageBckToJson(directoryPath);
+    }
+  });
 }
