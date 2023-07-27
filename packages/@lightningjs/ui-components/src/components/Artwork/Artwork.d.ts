27356f059218c240c0e7db7cf1d4a1822bdf2fd5
Mandy Wehrman <arwehrman@gmail.com>
2023-07-27 15:52:49 -0400

    fix(Artwork): updates types declaration (#304)
    * fix(Artwork): update type declaration

--- packages/@lightningjs/ui-components/src/components/Artwork/Artwork.d.ts
+++ packages/@lightningjs/ui-components/src/components/Artwork/Artwork.d.ts
@@ -16,13 +16,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import type lng from '@lightningjs/core';
-import type Base from '../Base';
-import type { Color, StylePartial } from '../../types/lui';
+import lng from '@lightningjs/core';
+import Base from '../Base';
+import { Color, StylePartial } from '../../types/lui';
 
 type AspectRatio = '16x9' | '3x4' | '4x3' | '2x1' | '1x1';
 
-export type ArtworkStyle = {
+type ArtworkStyle = {
   animationBlurEntrance: lng.types.TransitionSettings.Literal;
   animationBlurExit: lng.types.TransitionSettings.Literal;
   animationComponentEntrance: lng.types.TransitionSettings.Literal;
@@ -50,16 +50,122 @@ export type ArtworkStyle = {
   };
 };
 
-export default class Artwork extends Base {
+declare namespace Artwork {
+  export interface TemplateSpec extends Base.TemplateSpec {
+    /**
+     * When true, a blur will be applied to the image this will default to true if circleImage is true
+     */
+    blur?: boolean;
+    /**
+     * When true, a fill will be applied over the image this will default to false
+     */
+    fill?: boolean;
+    /**
+     * Sets the fallback image that will be shown in if the src request fails
+     */
+    fallbackSrc?: string;
+    /**
+     * Height value for the foregroundSrc if defined
+     */
+    foregroundH?: number;
+    /**
+     * Sets the fallback image that will be shown in if the src request fails
+     */
+    foregroundSrc?: string;
+    /**
+     * Width value for the foregroundSrc if defined
+     */
+    foregroundW?: number;
+    /**
+     * When true, a gradient will be added over all other elements
+     */
+    gradient?: boolean;
+    /**
+     * When true, scale will be applied over the image and this will default to false
+     */
+    shouldScale?: boolean;
+    /**
+     * Selection of image display modes
+     */
+    format?: 'default' | 'circle' | 'square' | 'contain';
+    /**
+     * Which image will be displayed
+     */
+    src: string; // Inherited from lng.Element
+    /**
+     * optional callback that can be used to generate custom strings to request an image. The callback will be passed an object containing the following parameters: aspectRatio, src, w, h. Be default aspect ratio will match the closest value from srcCallbackAspectRatios
+     */
+    srcCallback?: (obj: {
+      closestAspectRatio: string;
+      aspectRatio: string;
+      src: string;
+      w: number;
+      h: number;
+    }) => string;
+    /**
+     * srcCallback will receive an aspectRatio parameter when called, this array is used to find the closest value for your use case
+     */
+    srcCallbackAspectRatios?: AspectRatio[];
+  }
+  export interface TypeConfig extends lng.Component.TypeConfig {
+    SignalMapType: SignalMap;
+  }
+  /**
+   * emitted on initial loading of image
+   */
+  export type SignalMap = {
+    resolveLoading(): void;
+  };
+}
+
+declare class Artwork<
+  TemplateSpec extends Artwork.TemplateSpec = Artwork.TemplateSpec,
+  TypeConfig extends Artwork.TypeConfig = Artwork.TypeConfig
+> extends Base<TemplateSpec, TypeConfig> {
+  // Properties
+  /**
+   * When true, a blur will be applied to the image this will default to true if circleImage is true
+   */
   blur?: boolean;
+  /**
+   * When true, a fill will be applied over the image this will default to false
+   */
+  fill?: boolean;
+  /**
+   * Sets the fallback image that will be shown in if the src request fails
+   */
   fallbackSrc?: string;
+  /**
+   * Height value for the foregroundSrc if defined
+   */
   foregroundH?: number;
+  /**
+   * Image location for item that should be placed on top of the artwork above all other layers
+   */
   foregroundSrc?: string;
+  /**
+   * Width value for the foregroundSrc if defined
+   */
   foregroundW?: number;
+  /**
+   * When true, a gradient will be added over all other elements
+   */
   gradient?: boolean;
+  /**
+   * When true, scale will be applied over the image and this will default to false
+   */
   shouldScale?: boolean;
-  mode?: 'default' | 'circle' | 'square' | 'contain';
+  /**
+   * Selection of image display modes
+   */
+  format?: 'default' | 'circle' | 'square' | 'contain';
+  /**
+   * Which image will be displayed
+   */
   src: string; // Inherited from lng.Element
+  /**
+   * optional callback that can be used to generate custom strings to request an image. The callback will be passed an object containing the following parameters: aspectRatio, src, w, h. Be default aspect ratio will match the closest value from srcCallbackAspectRatios
+   */
   srcCallback?: (obj: {
     closestAspectRatio: string;
     aspectRatio: string;
@@ -67,7 +173,12 @@ export default class Artwork extends Base {
     w: number;
     h: number;
   }) => string;
+
+  /**
+   * srcCallback will receive an aspectRatio parameter when called, this array is used to find the closest value for your use case
+   */
   srcCallbackAspectRatios?: AspectRatio[];
+
   get style(): ArtworkStyle;
   set style(v: StylePartial<ArtworkStyle>);
 
@@ -80,3 +191,5 @@ export default class Artwork extends Base {
   get _Image(): lng.Component;
   get _Item(): lng.Component;
 }
+
+export { Artwork as default, ArtworkStyle };
