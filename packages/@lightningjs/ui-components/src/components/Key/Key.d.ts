2c3f3241fb1564b9115b87918f52d501eb38ef89
Loka <lokareddy222@gmail.com>
2023-07-30 09:15:53 -0400

    feat: update ts declaration file

--- packages/@lightningjs/ui-components/src/components/Key/Key.d.ts
+++ packages/@lightningjs/ui-components/src/components/Key/Key.d.ts
@@ -16,8 +16,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import lng from '@lightningjs/core';
 import Button, { ButtonStyle } from '../Button';
-import type { StylePartial } from '../../types/lui';
+import { StylePartial } from '../../types/lui';
 
 type SizeObject = {
   sm: number;
@@ -27,17 +28,85 @@ type SizeObject = {
   xxl: number;
 };
 
-export type KeyStyle = ButtonStyle & {
+type KeyStyle = ButtonStyle & {
   h: number;
   sizes: SizeObject;
   iconW: number;
   iconH: number;
 };
 
-export default class Key extends Button {
+declare namespace Key {
+  export interface TemplateSpec extends Button.TemplateSpec {
+    /**
+     * url for icon
+     */
+    icon?: string;
+
+    /**
+     * it allows custom handling of the "Enter" key press behavior in the Key component, overriding the default $onSoftKey and $toggleKeyboard events.
+     */
+    onEnter?: (key: string, toggle: boolean) => void;
+
+    /**
+     * width of the Key
+     */
+    size?: string;
+
+    /**
+     * used to toggle `$toggleKeyboard`
+     */
+    toggle?: boolean;
+
+    style?: StylePartial<KeyStyle>;
+  }
+  export interface TypeConfig extends lng.Component.TypeConfig {
+    SignalMapType: SignalMap;
+  }
+  /**
+   * Signal map interface for Key component
+   */
+  export type SignalMap = {
+    /**
+     * This signal is fired when the enter key is pressed while the Key component is focused.
+     * @param key The key value that triggered the event.
+     * @param toggle The toggle value of the `Key` component.
+     */
+    onSoftKey(key: string, toggle: boolean): void;
+
+    /**
+     *  This signal is fired when the enter key is pressed while the Key component is focused, and its toggle property is set to true
+     * @param key The key value that triggered the event.
+     * @param toggle The toggle value of the `Key` component.
+     */
+    toggleKeyboard(key: string, toggle: boolean): void;
+  };
+}
+declare class Key<
+  TemplateSpec extends Key.TemplateSpec = Key.TemplateSpec,
+  TypeConfig extends lng.Component.TypeConfig = lng.Component.TypeConfig
+> extends Button<TemplateSpec, TypeConfig> {
+  /**
+   * description text
+   */
   icon?: string;
-  size?: 'sm' | 'md' | 'lg' | 'xl';
-  toggle: string;
+
+  /**
+   * it allows custom handling of the "Enter" key press behavior in the Key component, overriding the default $onSoftKey and $toggleKeyboard events.
+   */
+  onEnter?: (key: string, toggle: boolean) => void;
+
+  /**
+   * Logo to be placed to the left of the title and description
+   */
+  size?: string;
+
+  /**
+   * used to toggle `$toggleKeyboard`
+   */
+  toggle?: boolean;
+
   get style(): KeyStyle;
   set style(v: StylePartial<KeyStyle>);
 }
+
+export { Key as default, KeyStyle, SizeObject };
