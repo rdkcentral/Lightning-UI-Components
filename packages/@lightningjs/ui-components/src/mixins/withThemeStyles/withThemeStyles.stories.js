c7fe80e0c1943aa9602583910c5ce1104e2e156f
Josh Howenstine <joshhowenstine@gmail.com>
2023-08-01 13:05:34 -0700

    fix: update withThemeStyles story. Memoize getStyleChain

--- packages/@lightningjs/ui-components/src/mixins/withThemeStyles/withThemeStyles.stories.js
+++ packages/@lightningjs/ui-components/src/mixins/withThemeStyles/withThemeStyles.stories.js
@@ -36,7 +36,7 @@ export const withThemeStyles = () => {
     color: context.theme.color.fillNeutral
   };
 
-  class Box extends withThemeStylesMixin(lng.Component) {
+  class Box extends lng.Component {
     static get __componentName() {
       return 'Box';
     }
@@ -64,7 +64,7 @@ export const withThemeStyles = () => {
     static _template() {
       return {
         Box: {
-          type: Box
+          type: withThemeStylesMixin(Box)
         }
       };
     }
