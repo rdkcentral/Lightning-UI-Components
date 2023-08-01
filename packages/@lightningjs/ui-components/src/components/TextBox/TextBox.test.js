af2afc24ec3f728a2e501c98606bce1dd7e73226
slhay28 <89874756+slhay28@users.noreply.github.com>
2023-08-01 16:42:23 -0400

    fix(TextBox): functionality add to tone property (#306)
    * fix(TextBox): functionality add to tone property
    
    * fix(textBox): fixing style guide
    
    * test(TextBox): removal of textColor test
    
    * test(TextBox): prettier style command
    
    * test(TextBox): updating snapshots

--- packages/@lightningjs/ui-components/src/components/TextBox/TextBox.test.js
+++ packages/@lightningjs/ui-components/src/components/TextBox/TextBox.test.js
@@ -155,12 +155,6 @@ describe('TextBox', () => {
       );
     });
 
-    it('should set textColor to the default white if no textColor is provided', () => {
-      textBox.content = 'Hello World!';
-      testRenderer.forceAllUpdates();
-      expect(textBox._Text.text.textColor).toBe(getValidColor('#FFFFFF'));
-    });
-
     it('should set textColor to the provided textColor value if defined', () => {
       textBox.content = 'Hello World!';
       textBox.style.textStyle.textColor = getValidColor('#000000');
