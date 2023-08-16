6dd42158f9e3a67f6870fba9b79e81e4f6310847
Tim Hojnoski <timhojnoski@gmail.com>
2023-08-16 09:57:05 -0400

    feat(Keyboard): add updated TS definitions

--- packages/@lightningjs/ui-components/src/components/Keyboard/Keyboard.d.ts
+++ packages/@lightningjs/ui-components/src/components/Keyboard/Keyboard.d.ts
@@ -24,7 +24,7 @@ type inputStyleObject = {
   radius: lng.Tools.CornerRadius;
 };
 
-export type KeyboardStyle = {
+type KeyboardStyle = {
   keySpacing: number;
   screenW: number;
   marginX: number;
@@ -32,12 +32,119 @@ export type KeyboardStyle = {
   inputStyle: inputStyleObject;
 };
 
-export default class Keyboard extends Base {
+type KeyboardFormat = Record<
+  string,
+  Array<Array<string | Record<string, unknown>>>
+>;
+
+declare namespace Keyboard {
+  export interface TemplateSpec extends Base.TemplateSpec {
+    // properties
+    formats?: KeyboardFormat[];
+    centerKeyboard?: boolean;
+    rowWrap?: boolean;
+    centerKeys?: boolean;
+    keyComponent?: lng.Component;
+    defaultFormat?: string;
+    columnCount?: number;
+    rowCount?: number;
+  }
+
+  export interface TypeConfig extends lng.Component.TypeConfig {
+    SignalMapType: SignalMap;
+  }
+  /**
+   * emitted when the width of the Keyboard changes in response to a child component's dimensions changing (triggered by a signal called '$itemChanged')
+   */
+  export type SignalMap = {
+    keyboardWidthChanged(): void;
+  };
+}
+
+type KeyboardSelectedKeyPosition = {
+  row: number;
+  column: number;
+};
+
+declare class Keyboard<
+  TemplateSpec extends Keyboard.TemplateSpec = Keyboard.TemplateSpec,
+  TypeConfig extends Keyboard.TypeConfig = Keyboard.TypeConfig
+> extends Base<TemplateSpec, TypeConfig> {
+  // Properties
+  /**
+   * object containing arrays that represent different formats that the keyboard can be presented in. These arrays can contain strings or objects.
+   */
+  formats?: KeyboardFormat[];
+
+  /**
+   * center the keyboard within it's set width (must set the w property of Keyboard)
+   */
   centerKeyboard?: boolean;
-  centerKeys?: boolean;
-  formats?: lng.Component | Array<lng.Component>;
-  keyComponent?: lng.Component;
+
+  /**
+   * wrap keyboard rows when navigating left and right
+   */
   rowWrap?: boolean;
+
+  /**
+   * center the keys within it's set width (must set the w property of Keyboard)
+   */
+  centerKeys?: boolean;
+
+  /**
+   * component to display for each key
+   */
+  keyComponent?: lng.Component;
+
+  /**
+   * Default format of the keyboard to be shown. Should be a key of `formats`.
+   */
+  defaultFormat?: string;
+
+  /**
+   * number of columns across the keyboard if passing a flat array
+   */
+  columnCount?: number;
+
+  /**
+   * number of rows down the keyboard if passing a flat array
+   */
+  rowCount?: number;
+
   get style(): KeyboardStyle;
   set style(v: StylePartial<KeyboardStyle>);
+
+  // methods
+  /**
+   * switches the keyboard format used to a newly specified format
+   * @param KeyboardFormat keyboard format to switch to
+   */
+  $toggleKeyboard(next: KeyboardFormat): void;
+
+  /**
+   *
+   * @param keyboard an element rendering a keyboard
+   * @param KeyboardSelectedKeyPosition an object specifying the row and column of the key to select
+   */
+  selectKeyOn(
+    keyboard: lng.Component,
+    KeyboardSelectedKeyPosition: KeyboardSelectedKeyPosition
+  ): void;
+
+  /**
+   * returns an object specifying the row and column of the selected key
+   */
+  getSelectedKey(): KeyboardSelectedKeyPosition;
+
+  /**
+   * An method triggered by fireAncestors('$itemChanged') being invoked by a child component.
+   */
+  $itemChanged(): void;
 }
+
+export {
+  Keyboard as default,
+  KeyboardStyle,
+  KeyboardSelectedKeyPosition,
+  KeyboardFormat
+};
