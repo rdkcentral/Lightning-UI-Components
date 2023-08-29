fd7e4ae288e3dbbfd8215082f813c43ca27bd44c
Mandy Wehrman <arwehrman@gmail.com>
2023-08-29 12:16:36 -0400

    feat(Storybook): removes comment typo in license block

--- bin/templates/docs.template.js
+++ bin/templates/docs.template.js
@@ -35,7 +35,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-SPDX-License-Identifier: Apache-2.0\*/}
+SPDX-License-Identifier: Apache-2.0 */}
 
 import { Meta, Title } from '@storybook/blocks';
 import * ${name}Stories from './${name}.stories';
