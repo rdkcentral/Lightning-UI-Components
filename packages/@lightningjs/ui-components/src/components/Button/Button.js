bf86ca22f53c0523f5d7bf38c20d5cfdf924b1a6
Emily Rautenberg <erautenb@gmail.com>
2023-10-27 15:29:02 -0400

    fix(Button): calculate height based on lineHeight and paddingY

--- packages/@lightningjs/ui-components/src/components/Button/Button.js
+++ packages/@lightningjs/ui-components/src/components/Button/Button.js
@@ -260,6 +260,11 @@ export default class Button extends Surface {
     if (newWidth !== this.w) {
       this.w = newWidth;
     }
+
+    this.h =
+      this.style.height ||
+      this.style.textStyle.lineHeight + this.style.paddingY * 2;
+
     // TODO breaks row resizing if this is wrapped in the above conditional
     this.fireAncestors('$itemChanged');
   }
