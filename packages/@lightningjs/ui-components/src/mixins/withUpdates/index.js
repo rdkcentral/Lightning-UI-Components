30e492fc18535f71dd0e8815668a7e01ad7a557c
Josh Howenstine <joshhowenstine@gmail.com>
2023-11-02 13:35:52 -0700

    fix: update deep merge to accomidate styles for componentConfig sub properties

--- packages/@lightningjs/ui-components/src/mixins/withUpdates/index.js
+++ packages/@lightningjs/ui-components/src/mixins/withUpdates/index.js
@@ -40,7 +40,14 @@ function getPropertyDescriptor(name, key) {
         if (changeHandler && typeof changeHandler === 'function') {
           value = changeHandler.call(this, value);
         }
-        this[key] = key === 'style' ? clone(this[key], value) : value;
+        const newValue = key === 'style' ? clone(this[key], value) : value;
+        
+        if (typeof this[key] === 'object' && this[key] !== null && this[key].style) {
+          const style = clone(this[key].style, value.style || {});
+          newValue.style = style;
+        }
+
+        this[key] = newValue;
         this.queueRequestUpdate();
       }
     },
