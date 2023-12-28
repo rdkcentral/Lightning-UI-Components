1514806c5888c9749112b55dc5fdc80642b6cc85
Aaron Begley <aaron.b99@gmail.com>
2023-12-28 08:25:38 -0800

    fix(MetadataCardContent): Fix default values

--- packages/@lightningjs/ui-components/src/components/MetadataCardContent/MetadataCardContent.stories.js
+++ packages/@lightningjs/ui-components/src/components/MetadataCardContent/MetadataCardContent.stories.js
@@ -20,6 +20,8 @@ import lng from '@lightningjs/core';
 import { default as MetadataCardContentComponent } from '.';
 import xfinityProviderLogoSquare from '../../assets/images/Xfinity-Provider-Logo-Square.png';
 import { createModeControl } from '../../docs/utils';
+import { getHexColor } from '../../utils';
+import lightningbolt from '../../assets/images/ic_lightning_white_32.png';
 
 export default {
   title: 'Components/MetadataCardContent'
@@ -46,9 +48,20 @@ MetadataCardContent.args = {
   w: 600,
   h: 250,
   title: 'Title',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  details: 'Details',
+  description: descriptionSample,
+  descriptionDetails: [
+    '94%',
+    {
+      icon: lightningbolt,
+      style: { color: getHexColor('00ff00') },
+      title: 'Green Lightning Bolt'
+    },
+    '86%',
+    {
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Tomato-Torrent-Icon.png',
+      title: 'Rotten Tomatoes rating'
+    }
+  ],details: 'Details',
   visibleCount: 3,
   mode: 'unfocused'
 };
@@ -59,49 +72,56 @@ MetadataCardContent.argTypes = {
     control: 'number',
     description: 'Width of component',
     table: {
-      defaultValue: { summary: 0 }
+      defaultValue: { summary: 0 },
+      type: { summary: 'number' }
     }
   },
   h: {
     control: 'number',
     description: 'Height of component',
     table: {
-      defaultValue: { summary: 0 }
+      defaultValue: { summary: 0 },
+      type: { summary: 'number' }
     }
   },
   title: {
     control: 'text',
     description: 'Title text',
     table: {
-      defaultValue: { summary: 'undefined' }
+      defaultValue: { summary: 'undefined' },
+      type: { summary: 'string' }
     }
   },
   description: {
     control: 'text',
     description: 'Description text directly below title',
     table: {
-      defaultValue: { summary: 'undefined' }
+      defaultValue: { summary: 'undefined' },
+      type: { summary: 'string' }
     }
   },
   descriptionDetails: {
     control: 'object',
-    description: 'Description details directly under the details text',
+    description: 'Description details directly below the description text',
     table: {
-      defaultValue: { summary: 'undefined' }
+      defaultValue: { summary: 'undefined' },
+      type: { summary: 'string' }
     }
   },
   details: {
     control: 'text',
     description: 'Details text at bottom left of componentDetails text',
     table: {
-      defaultValue: { summary: 'undefined' }
+      defaultValue: { summary: 'undefined' },
+      type: { summary: 'string' }
     }
   },
   visibleCount: {
     control: { type: 'range', min: 1, max: 10, step: 1 },
     description: 'Number of visible providers',
     table: {
-      defaultValue: { summary: 1 }
+      defaultValue: { summary: 1 },
+      type: { summary: 'number' }
     }
   }
 };
