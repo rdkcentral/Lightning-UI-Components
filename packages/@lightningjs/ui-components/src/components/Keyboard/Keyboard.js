0f7b6c331ddc070b67cf95dbb26e15cf546b13ee
slhay28 <89874756+slhay28@users.noreply.github.com>
2024-02-02 10:21:16 -0500

    fix(Keyboard): setting selectedIndex when creating rows (#462)

--- packages/@lightningjs/ui-components/src/components/Keyboard/Keyboard.js
+++ packages/@lightningjs/ui-components/src/components/Keyboard/Keyboard.js
@@ -130,6 +130,7 @@ export default class Keyboard extends Base {
           itemSpacing: this.style.keySpacing
         },
         items: this._createKeys(keys, keyboard),
+        selectedIndex: this._currentKeyboard?.selected?.selectedIndex || 0,
         waitForDimensions: true
       };
     });
