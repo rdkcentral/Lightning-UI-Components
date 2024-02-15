2b58e85f8e1f86a8f9d062ca3df024686db080d3
ChrisArasin <chris.arasin@gmail.com>
2024-02-15 09:58:10 -0500

    fix(Storybook): storybook updates more cleanly
    fix(Storybook): set theme and props before render
    
    fix(Storybook): evaluate args conditionals

--- packages/apps/lightning-ui-docs/.storybook/utils/registerEvents.js
+++ packages/apps/lightning-ui-docs/.storybook/utils/registerEvents.js
@@ -36,7 +36,10 @@ export const themeSelect = theme => {
       targetTheme = {};
       break;
   }
-  return context.setTheme(targetTheme);
+
+  return targetTheme.name && context.theme.name !== targetTheme.name
+    ? context.setTheme(targetTheme)
+    : Promise.resolve();
 };
 
 // registers all window events needed on load
