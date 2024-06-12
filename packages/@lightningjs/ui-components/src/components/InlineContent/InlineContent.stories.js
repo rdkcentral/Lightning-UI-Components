b09a9efa0f2a022991caaee467d1139aee08be86
Shayna Hay <shaynahay2023@gmail.com>
2024-06-12 10:04:57 -0400

    fix(InlineContent): updates test

--- packages/@lightningjs/ui-components/src/components/InlineContent/InlineContent.stories.js
+++ packages/@lightningjs/ui-components/src/components/InlineContent/InlineContent.stories.js
@@ -22,6 +22,9 @@ import lightningbolt from '../../assets/images/ic_lightning_white_32.png';
 import { getHexColor } from '../../utils';
 import TextBox from '../TextBox';
 
+const lorum =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales est eu eleifend interdum. Vivamus egestas maximus elementum. Sed condimentum ligula justo, non sollicitudin lectus rutrum vel. Integer iaculis vitae nisl quis tincidunt. Sed quis dui vehicula, vehicula felis a, tempor leo. Fusce tincidunt, ante eget pretium efficitur, libero elit volutpat quam, sit amet porta tortor odio non ligula. Ut sed dolor eleifend massa auctor porttitor eget ut lectus. Vivamus elementum lorem mauris, eu luctus tortor posuere sit amet. Nunc a interdum metus.';
+
 export default {
   title: 'Components/InlineContent',
   args: {
@@ -233,9 +236,6 @@ WithTruncation.args = {
   maxLines: 2,
   maxLinesSuffix: '...'
 };
-
-const lorum =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales est eu eleifend interdum. Vivamus egestas maximus elementum. Sed condimentum ligula justo, non sollicitudin lectus rutrum vel. Integer iaculis vitae nisl quis tincidunt. Sed quis dui vehicula, vehicula felis a, tempor leo. Fusce tincidunt, ante eget pretium efficitur, libero elit volutpat quam, sit amet porta tortor odio non ligula. Ut sed dolor eleifend massa auctor porttitor eget ut lectus. Vivamus elementum lorem mauris, eu luctus tortor posuere sit amet. Nunc a interdum metus.';
 
 // ADDED FOR TESTING PURPOSES, REMOVE BEFORE MERGE
 export const TextBoxVSInlineContent = args =>
