f7647a3157a61f2e4c90890fbf2f44e8573ffb87
David Richards, Jr. <dbarichardsjr@gmail.com>
2024-06-11 21:11:42 -0400

    fix(FocusManager): update docs and TS defs

--- packages/@lightningjs/ui-components/src/components/FocusManager/FocusManager.d.ts
+++ packages/@lightningjs/ui-components/src/components/FocusManager/FocusManager.d.ts
@@ -60,6 +60,16 @@ declare namespace FocusManager {
      * y (vertical) position value of the Row/Column (items array)
      */
     itemPosY?: number;
+
+    /**
+     * The zIndex used for the selected item.
+     */
+    selectedZIndex?: number;
+
+    /**
+     * The zIndex used for non-selected items.
+     */
+    defaultZIndex?: number;
   }
   export interface TypeConfig extends lng.Component.TypeConfig {
     SignalMapType: SignalMap;
@@ -111,6 +121,16 @@ declare class FocusManager<
    * y (vertical) position value of the Row/Column (items array)
    */
   itemPosY?: number;
+
+  /**
+   * The zIndex used for the selected item.
+   */
+  selectedZIndex?: number;
+
+  /**
+   * The zIndex used for non-selected items.
+   */
+  defaultZIndex?: number;
 
   // Accessors
 
